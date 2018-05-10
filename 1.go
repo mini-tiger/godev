2bcb5da14c001fa285f639c2e75e0884a5a70460
tj-thinkpad <61566027@163.com>
2018-05-10 16:51:26 +0800

    f

--- 1.go
+++ 1.go
@@ -1,46 +1,64 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
+	// "sync"
+	"runtime"
+	"time"
 )
 
+// var n sync.WaitGroup
+func sss1(ss1 chan<- int) {
+	// n.Add(1)
+	for i := 0; i < 10000000; i++ {
+
+		if i%2 == 0 && i%3 == 0 {
+			ss1 <- i
+		}
+		// ss2 <- ss1
+		// fmt.Println(<-ss2)
+
+	}
+	// n.Done()
+}
+func sss2(ss2 chan<- int) {
+	// n.Add(1)
+	for i := 0; i < 10000000; i++ {
+		if i%2 == 0 && i%3 == 0 {
+			// fmt.Println(i)
+			ss2 <- i
+		}
+		// fmt.Println(<-ss1)
+	}
+	// n.Done()
+}
 func main() {
-	l := list.New() //创建一个新的list
-	for i := 0; i < 5; i++ {
-		l.PushBack(i)
-	}
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出list的值,01234
-	}
-	fmt.Println("")
-	fmt.Println(l.Front().Value) //输出首部元素的值,0
-	fmt.Println(l.Back().Value)  //输出尾部元素的值,4
-	l.InsertAfter(6, l.Front())  //首部元素之后插入一个值为10的元素
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出list的值,061234
-	}
-	fmt.Println("")
-	l.MoveBefore(l.Front().Next(), l.Front()) //首部两个元素位置互换
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出list的值,601234
-	}
-	fmt.Println("")
-	l.MoveToFront(l.Back()) //将尾部元素移动到首部
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出list的值,460123
-	}
-	fmt.Println("")
-	l2 := list.New()
-	l2.PushBackList(l) //将l中元素放在l2的末尾
-	for e := l2.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出l2的值,460123
-	}
-	fmt.Println("")
-	l.Init()           //清空l</span>
-	fmt.Print(l.Len()) //0
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value) //输出list的值,无内容
+	runtime.GOMAXPROCS(2)
+	fmt.Println(runtime.GOOS, runtime.GOARCH)
+	// var s1 chan int = make(chan int)
+	// var s2 chan int = make(chan int)
+	// // s2 <- "s2"
+	// go sss1(s1)
+	// // go func(ss1 chan int) {
+	// // 	for i := 0; i < 100; i++ {
+
+	// // 		fmt.Println(<-ss1)
+	// // 	}
+	// // }(s1)
+	// go func() {
+	// 	for {
+
+	// 		fmt.Println(<-s1, <-s2)
+	// 	}
+	// }()
+	// go sss2(s2)
+
+	// // n.Wait()
+	// time.Sleep(5 * time.Second)
+	for a := 0; a < 10; a++ {
+		go fmt.Print(0)
+		time.Sleep(time.Second * 1)
+		fmt.Print(1)
 	}
 
 }
