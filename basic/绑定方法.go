6c1b8ec0db52a8d4b467e237a4c974ed83948bc6
taojun <61566027@163.com>
2018-08-18 20:36:41 +0800

    f

--- basic/绑定方法.go
+++ basic/绑定方法.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+type age int
+//type name string
+
+func (self *age) print_info()   {
+	fmt.Printf("age: %v",*self)
+}
+
+func main()  {
+	a1:=age(1)
+	fmt.Println(a1)
+	a1.print_info()
+
+
+
+}
