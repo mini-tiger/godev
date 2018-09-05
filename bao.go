7e222bc3ab5bc3bcce87f92ea7d5dce9e93619f2
taojun <61566027@163.com>
2018-09-05 18:16:51 +0800

    f

--- bao.go
+++ bao.go
@@ -6,22 +6,27 @@ import (
 	"os/exec"
 	"os"
 	"bytes"
-	log "github.com/sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 	"strconv"
 	"io"
 	"path/filepath"
 	"time"
+	"io/ioutil"
+	"bufio"
+	"syscall"
 	"fmt"
 )
 
 const filename = "_tmp.txt"
 
 type pan string
+var pf map[pan]uint64
+var os_type string = runtime.GOOS
 
-var pf map[pan]int64
+
 
 func win_run() {
-	pf = make(map[pan]int64, 0)
+	pf = make(map[pan]uint64, 0)
 	cmd := exec.Command("cmd", "/c", "wmic LOGICALDISK get name,size,freespace")
 	o, err := cmd.Output()
 	if err != nil {
@@ -46,7 +51,7 @@ func win_run() {
 		if len(temp_slice) != 3 {
 			continue
 		}
-		freesize, err := strconv.ParseInt(temp_slice[0], 10, 64)
+		freesize, err := strconv.ParseUint(temp_slice[0], 10, 64)
 		if err != nil {
 			log.Errorf("err:%s\n", err)
 		}
@@ -56,18 +61,19 @@ func win_run() {
 
 }
 
-func loop_write(f *os.File,freesize int64)  {
-	for i := int64(0); i < freesize; i++ {
+func loop_write(f *os.File,freesize uint64)  {
+	for i := uint64(0); i < freesize; i++ {
 		//ioutil.WriteFile(file,[]byte{65},0644)
 		f.Write([]byte{65})
 		time.Sleep(1*time.Second)
 	}
 }
 
-func create_file_sub(file string,freesize int64) {
+func create_file_sub(file string,freesize uint64) {
 	fmt.Println(file)
 	//create file
-	if _,err:=os.Stat(file);err!=nil{
+
+	if _,err:=os.Stat(file);err==nil{
 		if os.IsNotExist(err) {
 			f,err1:=os.Create(file)
 			if err1!=nil{
@@ -88,14 +94,21 @@ func create_file_sub(file string,freesize int64) {
 
 func create_file() {
 	for pan, freesize := range pf {
-		file := filepath.Join(string(pan),"\\", filename)
-		go create_file_sub(file, freesize)
+		if strings.Contains(os_type,"linux"){
+			file := filepath.Join(string(pan), filename)
+			//fmt.Println(file)
+			go create_file_sub(file, freesize)
+		}else{
+			file := filepath.Join(string(pan),"\\", filename)
+			go create_file_sub(file, freesize)
+		}
+
 	}
 }
 
 func main() {
-	os_type := runtime.GOOS
-	//fmt.Println(os)
+
+	//fmt.Println(os_type)
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel) //级别
 	log.SetFormatter(&log.TextFormatter{
@@ -104,5 +117,53 @@ func main() {
 		win_run()
 		create_file()
 	}
+	if strings.Contains(os_type,"linux"){
+		linux_run()
+		create_file()
+	}
 	select {}
+}
+
+func linux_run()  {
+	pf = make(map[pan]uint64, 0)
+	data,err:=ioutil.ReadFile("/proc/mounts")
+	if err!=nil {
+		if err!=io.EOF{
+			log.Errorf("linux /proc/mounts err:%s\n",err)
+		}
+
+	}
+	bb:=bytes.NewBuffer(data)
+	bf:=bufio.NewReader(bb)
+	for   {
+		line,err:=bf.ReadString('\n')
+		if err!=nil{
+			if err!=io.EOF{
+				log.Errorf("linux /proc/mounts err:%s\n",err)
+			}
+			break
+		}
+		tmp_s:=strings.Fields(line)
+		if strings.Contains(tmp_s[0],"/dev"){
+			//fmt.Println(tmp_s)
+
+			pf[pan(tmp_s[1])] = linux_freesize(tmp_s[1])
+		}
+	}
+
+	}
+func linux_freesize(pan string) (freeze uint64) {
+
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(pan, &fs)
+	if err != nil {
+		return
+	}
+	All := fs.Blocks * uint64(fs.Bsize)
+	Free := fs.Bfree * uint64(fs.Bsize)
+	Used := All - Free //unit   byte
+	//fmt.Println(Used)
+	freeze=Used
+	return
+
 }
