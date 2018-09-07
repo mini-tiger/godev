7537bf152b47ed0028806742be93cea044be9b79
tj-thinkpad <61566027@163.com>
2018-09-07 15:11:50 +0800

    f

--- bao.go
+++ bao.go
@@ -1,57 +1,117 @@
 package main
 
 import (
-	"path/filepath"
-	"os"
-	log "github.com/sirupsen/logrus"
-	"encoding/csv"
 	"fmt"
-	"io"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+	"time"
+	"net"
+	"log"
+	"os"
 )
 
-const filename = "Book1.csv"
+type sshinfo struct {
+	IP, Username string
+	Passwd       string
+	Port         int
+	client       *ssh.Client
+	Session      *ssh.Session
+	Result       string
+}
 
-func init() {
-	log.SetLevel(log.DebugLevel)
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true})
+func New_ssh(port int, args ...string) *sshinfo {
+	temp := new(sshinfo)
+	temp.Port = port
+	temp.IP = args[0]
+	temp.Username = args[1]
+	temp.Passwd = args[2]
+	return temp
+
+}
+func (cli *sshinfo) connect() error {
+	auth := make([]ssh.AuthMethod, 0)
+	auth = append(auth, ssh.Password(cli.Passwd))
+
+	hostKeyCallbk := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+		return nil
+	}
+	clientConfig := &ssh.ClientConfig{
+		User:            cli.Username,
+		Auth:            auth,
+		Timeout:         30 * time.Second,
+		HostKeyCallback: hostKeyCallbk,
+	}
+
+	// connet to ssh
+	addr := fmt.Sprintf("%s:%d", cli.IP, cli.Port)
+
+	client, err := ssh.Dial("tcp", addr, clientConfig)
+	if err != nil {
+		return err
+	}
+
+	// create session
+	session, err := client.NewSession()
+	if err != nil {
+		defer cli.close_session()
+		return err
+	}
+	cli.Session = session
+	return nil
+}
+func (cli *sshinfo) close_session() {
+	cli.Session.Close()
 }
 
 func main() {
-	file := filepath.Join("c:\\", filename)
-	f, err := os.OpenFile(file, os.O_RDWR, 0666)
-	if err != nil {
-		log.Errorln(err)
-	}
-	readfile(f)
 
-	f, err = os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666) //写入文件，追加
+	ssh := New_ssh(22, []string{"192.168.43.12", "root", "root"}...)
+	fmt.Println(ssh)
+	err := ssh.connect()
 	if err != nil {
-		log.Errorln(err)
+		log.Fatal(err)
 	}
-	writefile(f)
+	//ssh.Session.Stdout=os.Stdout
+	//ssh.Session.Stderr=os.Stderr
+	//ssh.Session.Run("touch /root/1")
+	//ssh.Session.Run("ls /; ls /tmp")
+	//ssh.close_session() //todo session一次运行一次run
+
+	terminal_run(ssh.Session)
+	ssh.close_session()
+
 }
 
-func readfile(f *os.File) {
-	r := csv.NewReader(f)
-	//fmt.Println(r.ReadAll())//全部读取 二维切片，子切片是一行
-	for i := 1; ; i++ {
-		r, err := r.Read()
-		if err != nil || err == io.EOF {
-			break
-		}
-		fmt.Printf("第%d行是:%v\n", i, r)
-	}
-}
+func terminal_run(session *ssh.Session)  {
 
-func writefile(f *os.File) {
-	w := csv.NewWriter(f)
-	data := [][]string{
-		{"a"},
-		{"b"},
-	}
-	err := w.WriteAll(data)
+	fd := int(os.Stdin.Fd())
+	oldState, err := terminal.MakeRaw(fd)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
+	defer terminal.Restore(fd, oldState)
+
+	// excute command
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	session.Stdin = os.Stdin
+
+	termWidth, termHeight, err := terminal.GetSize(fd)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,     // enable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm-256color", termHeight, termWidth, modes); err != nil {
+		log.Fatal(err)
+	}
+
+	session.Run("top")
 }
