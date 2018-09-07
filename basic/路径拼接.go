044986ee8ff8faf7e04a69d38cc6d80213631083
tj-thinkpad <61566027@163.com>
2018-09-07 10:57:51 +0800

    f

---
+++ basic/路径拼接.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"path/filepath"
+	"os"
+	"fmt"
+	"runtime"
+)
+
+func file_info(file string)  {
+	f, err := os.Stat(file)
+	fmt.Println(file)
+	if os.IsNotExist(err) {
+		fmt.Println("no")
+	} else {
+		fmt.Println(f.Name())                  //文件名
+		fmt.Println(filepath.Dir(file))        //文件路径
+		fmt.Println(filepath.VolumeName(file)) //文件盘符
+	}
+}
+
+func main()  {
+	//file:="c:/_tmp.txt"
+	//temp,err:=os.Getwd()
+	//
+	//fmt.Println(strings.Split(temp,":"))
+	////a:=filepath.VolumeName(file)
+	// todo  windows 路径拼接 开头盘符要加 \\
+	os_type:=runtime.GOOS
+	if os_type =="windows" {
+		//file:=filepath.Join("c:\\","work","go-dev","log.txt")
+		file := filepath.Join("c:\\work", "go-dev", "log.txt")
+		file_info(file)
+
+	}
+	////aa:=fmt.Sprintf("%s%s","c:","1.txt")
+	//fmt.Println(filepath.ToSlash(file))
+
+	if os_type == "linux"{
+		//p:=os.Getenv("GOPATH")
+		//fmt.Println(p)
+		// todo linux 路径拼接 开头根目录要加 /
+		file := filepath.Join("/home/go", "src","godev", "bao.go")
+		file_info(file)
+	}
+
+}
