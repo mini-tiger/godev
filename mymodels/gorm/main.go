785db40a942e2f3687253b4b92ecd1fb15a39321
tj-thinkpad <61566027@163.com>
2018-09-10 13:22:04 +0800

    f

--- mymodels/gorm/main.go
+++ mymodels/gorm/main.go
@@ -33,9 +33,11 @@ func main()  {
 
 	var counters []EndpointCounter
 
-	eids:=fmt.Sprintf("(%d.%d)",517,141)
+	eids:=fmt.Sprintf("(%d.%d)",517,141) // 需要带括号的格式 in
 	dt := db.Table("endpoint_counter").Select("endpoint_id, counter, step, type").Where(fmt.Sprintf("endpoint_id IN %s", eids))
+	// Select where 级联调用
 	dt = dt.Where("counter regexp ?", strings.TrimSpace("df"))
+	//todo 将结果再次where，limit 分页用  ， scan将结果存入 counteres切片，每个元素按照Struct存储
 	dt = dt.Limit(50).Offset(0).Scan(&counters)
 	for _,v:=range counters  {
 		fmt.Printf("%+v\n",v)
