98444b7f0806317281fbedba8720608b359c3b4e
tj-thinkpad <61566027@163.com>
2018-09-11 15:24:44 +0800

    commit hbs

---
+++ basic/函数作为变量.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type A1 struct {
+	a []map[string]interface{}
+	b func(int)
+}
+
+func main()  {
+
+	a1 := A1{}
+	a1.a= make([]map[string]interface{},0)
+	a1.b= func(a int) {
+		fmt.Println(a)
+	}
+
+	fmt.Println(a1)
+	a1.b(22)
+}
