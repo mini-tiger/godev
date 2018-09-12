f1ea8cfb57883577bc5087cad0a33ce50b2c6fe6
tj-thinkpad <61566027@163.com>
2018-09-12 11:08:29 +0800

    commit hbs

---
+++ basic/类型断言，类型转换.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
+type ColorGroup struct {
+	ID     int
+	Name   string
+	Colors []string
+}
+
+func main() {
+
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	b, err := json.Marshal(group) //打包 json
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
+	fmt.Println()
+	var cc interface{}
+	json.Unmarshal(b, &cc) //解析JSON
+	fmt.Printf("%T,%+v\n", cc, cc)
+
+	//json 元素类型转换
+	for _, v := range cc.(map[string]interface{}) {
+		switch v.(type) {
+		case float64:
+			fmt.Println("float64")
+			v = v.(float64)
+			fmt.Printf("%v\n", v)
+		case string:
+			fmt.Println("string")
+			v = v.(string)
+			fmt.Printf("%v\n", v)
+		case []interface{}:
+			//fmt.Println(v)
+
+			strArray := make([]string, 0)
+			for _, arg := range v.([]interface{}) {
+				strArray = append(strArray, arg.(string))
+			}
+			fmt.Println(strArray)
+		default:
+			fmt.Println("default")
+			fmt.Printf("%T\n", v)
+		}
+
+	}
+}
