08428057531548b3e802382a83c20cd4b435dc47
taojun <taojun@home>
2018-10-13 20:53:10 +0800

    f

--- first.go
+++ first.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"github.com/toolkits/nux"
 )
 
 func main() {
-	cpumhz,err:=nux.CpuMHz()
-	if err!=nil{
-		fmt.Println(err)
-	}
-	fmt.Println(cpumhz)
-	cpucore:=runtime.NumCPU()
-	fmt.Println(cpucore)
+	//cpumhz,err:=nux.CpuMHz()
+	//if err!=nil{
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(cpumhz)
+	//cpucore:=runtime.NumCPU()
+	//fmt.Println(cpucore)
 	//cmd := exec.Command("/bin/bash", "-c", "grep -i processor /proc/cpuinfo |wc -l")
 	//buf, _ := cmd.CombinedOutput()
 	//cmd.Run()
 	//fmt.Println(string(buf)) // todo 统一返回
-}
+	m:=make(map[string]interface{},1)
+	m["1"]=1
+	fmt.Println(len(m))
+	}
