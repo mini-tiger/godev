b059dce51a0e4f62b8d6f969fd357aea0edb75ec
work-deepin <61566027@163.com>
2018-10-26 18:32:52 +0800

    commit check_send_cmdb

--- first.go
+++ first.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 	"runtime"
-
+	"github.com/toolkits/nux"
 )
 
 func main() {
-
+	cpumhz,err:=nux.CpuMHz()
+	if err!=nil{
+		fmt.Println(err)
+	}
+	fmt.Println(cpumhz)
 	cpucore:=runtime.NumCPU()
 	fmt.Println(cpucore)
-	cmd := exec.Command("/bin/bash", "-c", "grep -i processor /proc/cpuinfo |wc -l")
-	buf, _ := cmd.CombinedOutput()
-	cmd.Run()
-	fmt.Println(string(buf)) // todo 统一返回
+	//cmd := exec.Command("/bin/bash", "-c", "grep -i processor /proc/cpuinfo |wc -l")
+	//buf, _ := cmd.CombinedOutput()
+	//cmd.Run()
+	//fmt.Println(string(buf)) // todo 统一返回
 }
