d5ac3449b460d9fbcd7c6f345a07441d02caa538
work-deepin <61566027@163.com>
2018-10-26 15:10:55 +0800

    commit check_send_cmdb

--- first.go
+++ first.go
@@ -1,73 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
-	"github.com/tidwall/gjson"
+	"os/exec"
+	"runtime"
+
 )
 
-type s struct {
-	A int               `json:"a"`
-	B string            `json:"b"`
-	S []string          `json:"s"`
-	M map[string]string `json:"m"`
-}
-
 func main() {
-	ss := s{1, "1", []string{"1", "2"}, map[string]string{"a": "b"}}
-	jsondata, err := json.Marshal(ss)
-	fmt.Println(string(jsondata))
-	fmt.Println(err)
-	var cc interface{}
-	//ss1:=&s{}
-	json.Unmarshal(jsondata, &cc)
-	//jiexi(cc)
 
-	//r:=gjson.Get(string(jsondata),"m")
-	//fmt.Println(r.Index,r.Num,r.Raw,r.Type,r.Str)
-	r:=gjson.ParseBytes(jsondata).Get("m")
-	fmt.Println(r)
-}
-func jiexi1(v interface{}) {
-
-		vk := reflect.ValueOf(v)
-		switch vk.Kind() {
-		case reflect.String:
-			v1 := v.(string)
-			fmt.Printf("string %s",v1)
-		case reflect.Int:
-			v1 := v.(int)
-			fmt.Println(v1)
-		case reflect.Slice:
-			v1 := v.([]interface{})
-			for _, v2 := range v1 {
-				fmt.Println(v2)
-			}
-		case reflect.Map:
-			v1 := v.(map[string]interface{})
-			fmt.Println(v1)
-		}
-
-}
-func jiexi(v interface{}) {
-	for _, vv := range v.(map[string]interface{}) {
-		vk := reflect.ValueOf(vv)
-		switch vk.Kind() {
-		case reflect.String:
-			v1 := vv.(string)
-			fmt.Println(v1)
-		case reflect.Int:
-			v1 := vv.(int)
-			fmt.Println(v1)
-		case reflect.Slice:
-			v1 := vv.([]interface{})
-			for _, v2 := range v1 {
-				fmt.Println(v2)
-			}
-		case reflect.Map:
-			v1 := vv.(map[string]interface{})
-			fmt.Println(v1)
-		}
-	}
+	cpucore:=runtime.NumCPU()
+	fmt.Println(cpucore)
+	cmd := exec.Command("/bin/bash", "-c", "grep -i processor /proc/cpuinfo |wc -l")
+	buf, _ := cmd.CombinedOutput()
+	cmd.Run()
+	fmt.Println(string(buf)) // todo 统一返回
 }
