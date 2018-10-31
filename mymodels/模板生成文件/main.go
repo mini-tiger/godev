102919dc48b8282510830fddda9ae8f5df309136
work-deepin <61566027@163.com>
2018-10-31 09:54:44 +0800

    commit check_send_cmdb

--- mymodels/模板生成文件/main.go
+++ mymodels/模板生成文件/main.go
@@ -2,45 +2,45 @@ package main
 
 import (
 	"github.com/satori/go.uuid"
-	"log"
-	"fmt"
 	"text/template"
 	"os"
 )
 
 type Variables struct {
 	UUID string
-	biz int
+	Biz  int
 }
 
-func createConf(bizid int,filename string)  {
-	u,err:=uuid.NewV4()
-	if err!=nil{
-		log.Println(err)
-	}
-
-	t, err := template.ParseFiles("C:\\work\\go-dev\\src\\godev\\mymodels\\模板生成文件\\agent.json") //parse the html file homepage.html
+func createConf(bizid int, filename string) error {
+	t, err := template.ParseFiles("/home/go/src/godev/mymodels/模板生成文件/agent.json.tmp") //parse the html file homepage.html
 	if err != nil { // if there is an error
-		log.Print("template parsing error: ", err) // log it
+		//log.Print("template parsing error: ", err) // log it
+		return err
 	}
 	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755) //读写模式
 	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+		//log.Println(err)
+		return err
+		//os.Exit(1)
 	}
 
-	p := Variables{UUID:u.String(),biz:bizid}
+	u, err := uuid.NewV4()
+	if err != nil {
+		//log.Println(err)
+		return err
+	}
+	p := Variables{UUID: u.String(), Biz: bizid}
 	//if err := t.Execute(os.Stdout, p); err != nil {
 	//	fmt.Println("There was an error:", err.Error())
 	//}
 	if err := t.Execute(f, p); err != nil {
-		fmt.Println("There was an error:", err.Error())
+		//fmt.Println("There was an error:", err.Error())
+		return err
 	}
+	return nil
 
-
-
-	}
-func main()  {
-	createConf(3,"c:\\cfg.json")
+}
+func main() {
+	createConf(3, "/home/go/src/godev/mymodels/模板生成文件/cfg.json")
 
 }
