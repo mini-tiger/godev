1b3141256466180436467c898561f9ebb459e6d7
tj-thinkpad <61566027@163.com>
2018-11-01 16:25:46 +0800

    commit

--- models/sort.go
+++ models/sort.go
@@ -9,7 +9,7 @@ import (
 )
 
 func init() {
-
+	fmt.Println(int(time.Now().Unix()))
 	intList := []int{2, 4, 3, 5, 7, 6, 9, 8, 1, 0}
 	float8List := []float64{4.2, 5.9, 12.3, 10.0, 50.4, 99.9, 31.4, 27.81828, 3.14}
 	stringList := []string{"a", "c", "b", "d", "f", "i", "z", "x", "w", "y"}
