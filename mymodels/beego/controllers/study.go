052444fd8dca18bc4b98f51fa4321d1e26fd0929
tj-thinkpad <61566027@163.com>
2018-11-08 14:27:40 +0800

    commit

--- mymodels/beego/controllers/study.go
+++ mymodels/beego/controllers/study.go
@@ -13,8 +13,8 @@ type Study struct {
 }
 
 type User struct {
-	Name  interface{} `form:"username",json:"name"`
-	Age   int `form:"age",json:"age"`
+	Name interface{} `form:"username",json:"name"`
+	Age  int         `form:"age",json:"age"`
 }
 
 func (c *Study) Get() {
@@ -25,11 +25,11 @@ func (c *Study) Get() {
 
 	o1 := orm.NewOrm()
 	o1.Using("default")
-	fmt.Println(o1.Driver().Name()) // 数据库别名，default  切换不同数据库用
-	fmt.Println(o1.Driver().Type() == orm.DRMySQL)// 数据库类型
+	fmt.Println(o1.Driver().Name())                // 数据库别名，default  切换不同数据库用
+	fmt.Println(o1.Driver().Type() == orm.DRMySQL) // 数据库类型
 
-	user := models.User{Id:0} // todo 空数据库 先查询
-	err:=o1.Read(&user)
+	user := models.User{Id: 0} // todo 空数据库 先查询
+	err := o1.Read(&user)
 	if err == orm.ErrNoRows {
 		fmt.Println("查询不到")
 	} else if err == orm.ErrMissPK {
@@ -39,7 +39,7 @@ func (c *Study) Get() {
 	}
 
 	// todo 读 若不存在则创建
-	user = models.User{Name:"11",Email:"@111"}
+	user = models.User{Name: "11", Email: "@111"}
 	// 默认必须传入一个参数作为条件字段，同时也支持多个参数多个条件字段
 	// 三个返回参数依次为：是否新创建的，对象 Id 值，错误
 	if created, id, err := o1.ReadOrCreate(&user, "Name"); err == nil { // todo 通过Name作为判断是否有数据标准
@@ -51,13 +51,13 @@ func (c *Study) Get() {
 	}
 
 	// todo 根据 人 添加对应的多个 课程
-	c1:=models.Class{Name:"shuxue",User:&user}
+	c1 := models.Class{Name: "shuxue", User: &user}
 	id, err := o1.Insert(&c1)
 	if err == nil {
 		fmt.Println(id)
 	}
 
-	c2:=models.Class{Name:"yuwen",User:&user}
+	c2 := models.Class{Name: "yuwen", User: &user}
 	id, err = o1.Insert(&c2)
 	if err == nil {
 		fmt.Println(id)
@@ -65,14 +65,14 @@ func (c *Study) Get() {
 	//todo 查询所有user id大于0
 	fmt.Println("===========查询所有user id大于0===================")
 	var users []*models.User
-	num, err := o1.QueryTable("auth_user").Filter("id__gt",0).All(&users)
+	num, err := o1.QueryTable("auth_user").Filter("id__gt", 0).All(&users)
 	fmt.Printf("Returned Rows Num: %d, %s\n", num, err)
 
 	//todo 查询所有user  返回MAP
 	fmt.Println("===========查询所有user  返回MAP===================")
 	//uu:=map[string]interface{}
-	var users2 []orm.Params //这是 params是orm模块自带的，不是模型
-	num, err = o1.QueryTable("auth_user").Values(&users2,"name","email")// todo 可以分字段或者全部
+	var users2 []orm.Params                                                //这是 params是orm模块自带的，不是模型
+	num, err = o1.QueryTable("auth_user").Values(&users2, "name", "email") // todo 可以分字段或者全部
 	fmt.Printf("Returned Rows Num: %d, %s\n", num, err)
 	for _, m := range users2 {
 		fmt.Println(m["Name"], m["Email"]) //todo 这里要用大写
@@ -82,7 +82,7 @@ func (c *Study) Get() {
 	//  每个子元素都是一个切片，排列与 Model 中定义的 Field 顺序一致
 	fmt.Println("===========查询所有user  返回slice===================")
 	var lists []orm.ParamsList
-	num, err = o1.QueryTable("auth_user").ValuesList(&lists,"name","class__name")
+	num, err = o1.QueryTable("auth_user").ValuesList(&lists, "name", "class__name")
 	if err == nil {
 		fmt.Printf("Result Nums: %d\n", num)
 		fmt.Println(lists)
@@ -91,7 +91,7 @@ func (c *Study) Get() {
 	//todo 查询所有user  ,每个user 单个列 存入slice
 	fmt.Println("===========查询所有user  ,每个user 单个列 存入slice===================")
 	var lists1 orm.ParamsList
-	num, err = o1.QueryTable("class").ValuesFlat(&lists1,"name")
+	num, err = o1.QueryTable("class").ValuesFlat(&lists1, "name")
 	if err == nil {
 		fmt.Printf("Result Nums: %d\n", num)
 		fmt.Println(lists1)
@@ -104,7 +104,7 @@ func (c *Study) Get() {
 	if err == nil {
 		fmt.Printf("%d posts read\n", num)
 		for _, post := range clss {
-			fmt.Printf("Id: %d, Name: %s User:%+v,\n",post.Id, post.Name, post.User)
+			fmt.Printf("Id: %d, Name: %s User:%+v,\n", post.Id, post.Name, post.User)
 		}
 	}
 
@@ -112,27 +112,81 @@ func (c *Study) Get() {
 	// 方法一
 	fmt.Println("==============课程是数学的 属于哪个人===========")
 	var user11 []orm.Params
-	num,err = o1.QueryTable("auth_user").Filter("class__name","shuxue").Values(&user11)
+	num, err = o1.QueryTable("auth_user").Filter("class__name", "shuxue").Values(&user11)
 	if err == nil {
 		//fmt.Println(user11)
-		fmt.Printf("数学user:%+v 拥有\n",user11[0]["Name"])
+		fmt.Printf("数学user:%+v 拥有\n", user11[0]["Name"])
 	}
 	// 方法二
 	var cc1 models.Class
 	var user2 models.User
-    err=o1.QueryTable("class").Filter("name__icontains","shuxue").Limit(1).One(&cc1)
-	if err == nil{
+	err = o1.QueryTable("class").Filter("name__icontains", "shuxue").Limit(1).One(&cc1)
+	if err == nil {
 		fmt.Println(cc1)
-		o1.QueryTable("auth_user").Filter("id",cc1.User).Limit(1).One(&user2)
-		fmt.Printf("数学user:%+v 拥有\n",user2.Name)
+		o1.QueryTable("auth_user").Filter("id", cc1.User).Limit(1).One(&user2)
+		fmt.Printf("数学user:%+v 拥有\n", user2.Name)
 	}
 
-	// 级联删除 用户下的课程
-	o1.QueryTable("auth_user").Filter("id",1).Delete()
+	//todo 查询所有 分页
+	p := 3  // 当前是第几页
+	pp := 2 // 每页多少个
 
-	c.TplName = "study/index.tpl" //
+	fmt.Println("===========查询所有 分页===================")
+
+	var all []orm.Params             //这是 params是orm模块自带的，不是模型
+	qs := o1.QueryTable("auth_user") // 数据结果集
+	var off int
+	if p == 1 { // 如果第一页 偏移0个
+		off = 0
+	} else {
+		off = (p - 1) * pp // 不是第一页 页码-1   在 乘以 每页个数
+	}
+
+	var rp Page
+	rp.PageNo = p    // 当前页码
+	rp.PageSize = pp // 每页多少行
+	rc, err := qs.Count()
+	rp.TotalCount = int(rc) // 共多少行
+	if rp.TotalCount%rp.PageSize == 0 { // 如果总行数 能被 每页行数 除尽
+		rp.TotalPage = rp.TotalCount / rp.PageSize // 共有多少页
+	} else {
+		rp.TotalPage = rp.TotalCount/rp.PageSize + 1
+	}
+	rp.FirstPage = false
+	if rp.PageNo == 1 { // 如果当前页码是1  ，是首页
+		rp.FirstPage = true
+	}
+	rp.LastPage = false
+	if rp.TotalPage == rp.PageNo { // 如果当前页码和 总共页数相等，是最后页
+		rp.LastPage = true
+	}
+
+	qs.OrderBy("-update").Limit(pp).Offset(off).Values(&all)
+	fmt.Printf("Returned Rows Num: %+v\n", all)
+	//for _, m := range all {
+	//	fmt.Println(m["Name"], m["Update"]) //todo 这里要用大写
+	//}
+	rp.List = all // 当前页所有数据
+	fmt.Printf("%+v", rp)
+	c.Data["json"] = &rp
+	c.ServeJSON()		//	 返回JSON
+
+	// todo 级联删除 用户下的课程
+	//o1.QueryTable("auth_user").Filter("id",1).Delete()
+
+	// todo 返回HTML
+	//c.TplName = "study/index.tpl" //
 }
 
+type Page struct {
+	PageNo     int         // 当前页码
+	PageSize   int         // 每页多少行
+	TotalPage  int         // 共多少页
+	TotalCount int         // 共多少行
+	FirstPage  bool        // 是否第一页
+	LastPage   bool        // 是否最后一页
+	List       interface{} // 数据
+}
 
 func (c *Study) Post() {
 	//u := User{}
@@ -141,14 +195,14 @@ func (c *Study) Post() {
 	//}
 	//fmt.Println(u)
 	fmt.Println(string(c.Ctx.Input.RequestBody))
-	u:=User{}
+	u := User{}
 	json.Unmarshal(c.Ctx.Input.RequestBody, &u)
 	fmt.Println(u)
 
 	//c.Data["data1"] = "taojun"
 	//c.Data["data2"] = "this is test data"
 	//c.TplName = "study/index.tpl" //
-	c.Data["json"]=u
+	c.Data["json"] = u
 	c.ServeJSON()
 
 }
