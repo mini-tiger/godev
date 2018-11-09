5a9c9018a05444b8cfab5d830faad9d0955f7552
tj-thinkpad <61566027@163.com>
2018-11-09 18:05:31 +0800

    commit

--- mymodels/获取本机所有相关IP.go
+++ mymodels/获取本机所有相关IP.go
@@ -3,13 +3,13 @@ package main
 import (
 	"net"
 	"fmt"
-	"net/http"
-	"io/ioutil"
 	"log"
 	"runtime"
 	"sync"
 	"strings"
 	"time"
+	"github.com/toolkits/net/httplib"
+	"io/ioutil"
 )
 
 var L *sync.WaitGroup = new(sync.WaitGroup)
@@ -51,19 +51,30 @@ func get_external() {
 	//defer func() {
 	//	L.Done()
 	//}()
-	resp, err := http.Get("http://myexternalip.com/raw")
+	rq:=httplib.Get("http://myexternalip.com/raw").SetTimeout(time.Duration(5)*time.Second,time.Duration(5)*time.Second)
+	resp,err:=rq.Response()
 	if err != nil {
 		ip2 <- ""
 		errlog(err)
 	}
-	defer resp.Body.Close()
 	content, _ := ioutil.ReadAll(resp.Body)
-	//buf := new(bytes.Buffer)
-	//buf.ReadFrom(resp.Body)
-	//s := buf.String()
 	tmp := string(content)
 	tmp = strings.TrimSpace(tmp)
 	tmp = strings.Trim(tmp, "\n")
+
+	//resp, err := http.Get("http://myexternalip.com/raw")
+	//if err != nil {
+	//	ip2 <- ""
+	//	errlog(err)
+	//}
+	//defer resp.Body.Close()
+	//content, _ := ioutil.ReadAll(resp.Body)
+	////buf := new(bytes.Buffer)
+	////buf.ReadFrom(resp.Body)
+	////s := buf.String()
+	//tmp := string(content)
+	//tmp = strings.TrimSpace(tmp)
+	//tmp = strings.Trim(tmp, "\n")
 	ip2 <- tmp
 	//fmt.Println(string(content))
 }
