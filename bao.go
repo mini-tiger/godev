931e3ca7a3c48fb9812836719a086a36f70d07fe
tj-thinkpad <61566027@163.com>
2018-11-15 15:22:03 +0800

    commit

--- bao.go
+++ bao.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"reflect"
+	"unsafe"
 )
 
 func main()  {
-	m:=make(map[string]string,0)
-	if len(m) == 0{
-		fmt.Println(1)
-	}
-	fmt.Println(len(m))
-	m1:=[]map[string]string{}
-	fmt.Println(len(m1))
-
-
-	a:= func() {
-		for   {
-			fmt.Println(111222)
-			time.Sleep(time.Duration(1)*time.Second)
-		}
-
-	}
-	b:= func() {
-		go a()
-		for   {
-			fmt.Println(222333)
-			time.Sleep(time.Duration(1)*time.Second)
-		}
-
-	}
-	go b()
-
-	select {}
-
-
+	var a reflect.StringHeader
+	a.Data=unsafe.Pointer("123")
 }
