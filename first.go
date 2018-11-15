62d5e9ad310148d0e1bcf016c21626f69555c307
tj <61566027@163.com>
2018-11-15 22:58:29 +0800

    默认的

--- first.go
+++ first.go
@@ -1,28 +1,15 @@
 package main
 
-import "testing"
-import "unsafe"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
-func Test_ByteString(t *testing.T) {
-	var x = []byte("Hello World!")
-	var y = *(*string)(unsafe.Pointer(&x))
-	var z = string(x)
-
-	if y != z {
-		t.Fail()
-	}
-}
-
-func Benchmark_Normal(b *testing.B) {
-	var x = []byte("Hello World!")
-	for i := 0; i < b.N; i ++ {
-		_ = string(x)
-	}
-}
-
-func Benchmark_ByteString(b *testing.B) {
-	var x = []byte("Hello World!")
-	for i := 0; i < b.N; i ++ {
-		_ = *(*string)(unsafe.Pointer(&x))
-	}
+func main() {
+	s, e := os.Open("D:\\putty")
+	fmt.Println(e)
+	f, e := s.Stat()
+	fmt.Println(f.IsDir())
+	fmt.Println(filepath.Dir("putty\\PAGEANT.EXE"))
 }
