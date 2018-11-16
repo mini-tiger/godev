3172215d8d356b0d045dac2641b18e0d15692314
work-deepin <61566027@163.com>
2018-11-16 17:30:58 +0800

    commit check_send_cmdb

--- mymodels/运行系统命令/main.go
+++ mymodels/运行系统命令/main.go
@@ -7,27 +7,42 @@ import (
 	"fmt"
 	"os/exec"
 	"bytes"
+	"sort"
+	"time"
+	"strings"
 )
 
-
 type Monitor struct {
-	Cmd string `json:"cmd"`
-	Return int `json:"return"`
+	Cmd    string `json:"cmd"`
+	Return int    `json:"return"`
 }
 
 type InstallAll struct {
-	Interval    int          `json:"interval"`
-	SkipErr     bool         `json:"skipErr"`
+	Interval    int                 `json:"interval"`
+	StopErr     bool                `json:"stopErr"`
 	InstallStep map[int]interface{} `json:"install_step"`
 	StartStep   map[int]interface{} `json:"start_step"`
-
 }
 
 type Install struct {
 	InstallAll InstallAll `json:"install_all"`
-	Monitor Monitor `json:"monitor"`
+	Monitor    Monitor    `json:"monitor"`
 }
 
+type InstallResp struct {
+	Success bool
+	error   string
+	Step    string
+	cmd     string
+}
+
+func mapKeySort(m map[int]interface{}) (keys []int) {
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	return
+}
 
 func UnJson(jb []byte) (err error, dl *Install) {
 	err = json.Unmarshal(jb, &dl) //解析
@@ -39,37 +54,91 @@ func UnJson(jb []byte) (err error, dl *Install) {
 }
 
 func main() {
-	f, _ := ioutil.ReadFile("C:\\work\\go-dev\\src\\godev\\mymodels\\运行系统命令\\install.json")
-	err, dl := UnJson(f) // 解析Json
+	f, err := ioutil.ReadFile("/home/go/src/godev/mymodels/运行系统命令/install.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err, dataJson := UnJson(f) // 解析Json
 
 	if err != nil {
 		fmt.Println(1, err)
 	}
-	fmt.Println(dl)
-	fmt.Println(dl.Monitor)
-	fmt.Println(dl.InstallAll.InstallStep)
+	fmt.Println(dataJson)
+	fmt.Println(dataJson.Monitor)
 
+	cinstall := make(chan struct{}, 0)
 
-	//cinstall := make(chan struct{},0)
+	for _,step:=range []string{"installStep", "startStep"}{
+		resp,stop:=StepRun(step, dataJson, cinstall)
+		if stop{
+			fmt.Println(resp)
+			break
+		}else{
+			fmt.Println(11111,resp)
+		}
+	}
+	//resp:=StepRun("installStep", dataJson, cinstall)
+	//
+	//fmt.Println(resp)
+	//resp=StepRun("startStep", dataJson,cinstall)
+	////
+	////<-cinstall
+	////<-cinstall
+	//
+	//fmt.Println(resp)
 	//cstart := make(chan struct{},0)
 	//
 	//RunCommand()
 
 }
-func RunCommand(command string) (string,string) {
+
+func StepRun(step string, dl *Install, c chan struct{}) (resp InstallResp,stop bool) {
+	//defer func() {
+	//	c <- struct{}{}
+	//}()
+	var mapInstStep interface{}
+	switch step {
+	case "installStep":
+		mapInstStep = dl.InstallAll.InstallStep
+		mapInstStep = mapInstStep.(map[int]interface{})
+	case "startStep":
+		mapInstStep = dl.InstallAll.StartStep
+		mapInstStep = mapInstStep.(map[int]interface{})
+	}
+
+	mapInst := mapInstStep.(map[int]interface{})
+	slInstStep := mapKeySort(mapInst)
+
+	for _, v := range slInstStep {
+		cmd, _ := mapInst[v]
+		stderr, stdout := RunCommand(cmd.(string))
+		if stderr != "" && dl.InstallAll.StopErr {
+			resp.error = strings.TrimRight(stderr,"\n")
+			resp.cmd = cmd.(string)
+			resp.Step = step
+			resp.Success = false
+			stop=true
+			break
+		}
+		log.Printf("begin run Step:%s, index: %d,stdout:%s,stderr:%s\n", step, v,stdout, stderr)
+		time.Sleep(time.Duration(dl.InstallAll.Interval) * time.Second)
+	}
+	return
+}
+
+func RunCommand(command string) (string, string) {
 	cmd := exec.Command("/bin/bash", "-c", command)
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
 	var e bytes.Buffer
-	cmd.Stderr =&e
+	cmd.Stderr = &e
 	//cmd.Start()
 	//buf, _ := cmd.CombinedOutput()
 	cmd.Run()
-	if e.Len() != 0 && out.Len() == 0{
-		return e.String(),out.String()
+	if e.Len() != 0 && out.Len() == 0 {
+		return e.String(), out.String()
 	}
-	return "",out.String()
+	return "", out.String()
 }
-
