b07c05045e81330d075317fe6929869acfaad859
tj-thinkpad <61566027@163.com>
2018-12-14 18:17:10 +0800

    commit

--- first.go
+++ first.go
@@ -1,48 +1,39 @@
 package main
 
 import (
+	"time"
 	"fmt"
-	"net/http"
-
-	"github.com/silenceper/wechat"
-	"github.com/silenceper/wechat/message"
+	"compress/zlib"
 )
 
-func hello(rw http.ResponseWriter, req *http.Request) {
-
-	//配置微信参数
-	config := &wechat.Config{
-		AppID:          "1",
-		AppSecret:      "1YwqhYfsUKDoRYYJWjMRcL_T0G2ZSPCPmQ2RwhDTzcw",
-		Token:          "weixin",
-		EncodingAESKey: "BQptm8SueWbIj8z1NRPNSxdznzSAmRMiP54cKSmCsQh",
-	}
-	wc := wechat.NewWechat(config)
-
-	// 传入request和responseWriter
-	server := wc.GetServer(req, rw)
-	//设置接收消息的处理方法
-	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
-
-		//回复消息：演示回复用户发送的消息
-		text := message.NewText(msg.Content)
-		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
-	})
-
-	//处理消息接收以及回复
-	err := server.Serve()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//发送回复的消息
-	server.Send()
+type Test struct {
+	S string
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		fmt.Printf("start server error , err=%v", err)
+	timeLayout := "2006-01-02 15:04:05"
+	s := time.Now().Unix()
+	s = 1544600603
+	fmt.Println(time.Unix(s, 0).Format(timeLayout))
+	//time.Sleep(time.Duration(5)*time.Second)
+	ss := time.Now().Unix()
+	d,h:=getday(s, ss)
+	fmt.Printf("%d天%d小时",d,h)
+}
+func getday(sinter int64) (bb bool,day,interval int64) { // 是否不足1天, 有几天, 减去天数后的时间差
+	day =
+	if day < 0 {
+		b
 	}
+	return
+
+}
+
+func getBasic(sinter, tt int64) (zhengshu int64,b bool) { //除完的整数,是否整数大于0
+	zhengshu = sinter / tt
+	if zhengshu >0{
+		b=true
+		return
+	}
+	return
 }
