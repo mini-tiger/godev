6ce311f3cbc6c5c2a480233a3b46ffeb0bef6dd7
taojun <61566027@163.com>
2018-12-16 21:47:45 +0800

    f

--- first.go
+++ first.go
@@ -4,30 +4,34 @@ import (
 	"os/exec"
 	"strings"
 	"fmt"
+	"unsafe"
 )
 
-func getOSName() (cm string) {
-	cmd := exec.Command("/bin/bash", "-c", "lsb_release -d")
-	buf, _ := cmd.CombinedOutput()
-	cmd.Run()
-	cm = strings.Trim(string(buf), " ")
-	cm = strings.Trim(cm,"Description:")
-	cm = strings.Trim(cm, "\t")
-	cm = strings.Trim(cm, "\r\n")
-	return
+
+
+func TrimAny(cm *string) *string {
+	*cm = strings.Trim(*cm, " ")
+	*cm = strings.Trim(*cm, "\t")
+	*cm = strings.Trim(*cm, "\r\n")
+	return cm
 }
 
-func getOSVersion() (cm string) {
+func getOSVersion() (cm *string) {
 	cmd := exec.Command("/bin/bash", "-c", "lsb_release -r")
 	buf, _ := cmd.CombinedOutput()
 	cmd.Run()
-	cm = strings.Trim(string(buf), " ")
-	cm = strings.Trim(cm,"Release:")
-	cm = strings.Trim(cm, "\t")
-	cm = strings.Trim(cm, "\r\n")
-	return
+	//cm = strings.Trim(string(buf), " ")
+	cm = (*string)(unsafe.Pointer(&buf))
+	if strings.Contains(*cm,"not found"){
+
+	}else{
+		*cm=strings.Trim(*cm,"Release:")
+	}
+
+
+	return TrimAny(cm)
 }
 func main()  {
-	fmt.Print(getOSName())
-	fmt.Print(getOSVersion())
+	//fmt.Print(getOSName())
+	fmt.Print(*getOSVersion())
 }
