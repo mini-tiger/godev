7351ef1b487571ecbf4cd5f90027aa076b9368f6
tao-work-centos6.x <61566027@163.com>
2018-12-18 15:00:58 +0800

    f

--- mymodels/uuid,IpSn生成md5.go
+++ mymodels/uuid,IpSn生成md5.go
@@ -82,12 +82,48 @@ func main() {
 
 	fmt.Println(*ip)
 	fmt.Println(sn)
-	m := Md5(fmt.Sprintf("%s%s", *ip, sn)) // 生成md5 字符串 唯一
 
-	fmt.Println(m)
+	//m := Md5(fmt.Sprintf("%s%s", sn, *ip)) // todo ip,sn生成md5 字符串 唯一
+
+	mac := getMAC(*ip)
+	if mac == "" {
+		log.Println("mac err:")
+	}else {
+		fmt.Println("mac:",mac)
+	}
+
+
+	m := Md5(fmt.Sprintf("%s%s", mac, *ip)) // todo mac,ip生成md5 字符串 唯一
+
+	fmt.Println(m, len(m))
 	u3, err := uuid.FromString(m) // 生成UUID格式
 	if err != nil {
 		fmt.Printf("Something went wrong: %s\n", err)
 	}
 	fmt.Printf("Successfully parsed: %s\n", u3)
+}
+func getMAC(ips string) string {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic("Poor soul, here is what you got: " + err.Error())
+	}
+	for _, inter := range interfaces {
+
+		interaddr, _ := inter.Addrs()
+		for _, a := range interaddr {
+			//fmt.Println(a)
+			if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				ip := ipnet.IP.To4()
+				//fmt.Println(ip.String())
+				i := strings.Split(ip.String(),"/")
+				//fmt.Println(i)
+				if i[0] == ips {
+					//fmt.Println(ipnet.IP.String())
+					//fmt.Println(inter.Name, inter.HardwareAddr.String())
+					return inter.HardwareAddr.String()
+				}
+			}
+		}
+	}
+	return ""
 }
