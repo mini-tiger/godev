28fb5ed0981bfc6e14c6edfc87de125f8c298ada
tao-work-centos6.x <61566027@163.com>
2018-12-20 09:39:33 +0800

    f

--- first.go
+++ first.go
@@ -1,37 +1,15 @@
 package main
 
 import (
-	"os/exec"
-	"strings"
+	"github.com/pkg/errors"
 	"fmt"
-	"unsafe"
+	"strings"
 )
 
-
-
-func TrimAny(cm *string) *string {
-	*cm = strings.Trim(*cm, " ")
-	*cm = strings.Trim(*cm, "\t")
-	*cm = strings.Trim(*cm, "\r\n")
-	return cm
-}
-
-func getOSVersion() (cm *string) {
-	cmd := exec.Command("/bin/bash", "-c", "lsb_release -r")
-	buf, _ := cmd.CombinedOutput()
-	cmd.Run()
-	//cm = strings.Trim(string(buf), " ")
-	cm = (*string)(unsafe.Pointer(&buf))
-	if strings.Contains(*cm,"not found"){
-
-	}else{
-		*cm=strings.Trim(*cm,"Release:")
-	}
-
-
-	return TrimAny(cm)
-}
 func main()  {
-	//fmt.Print(getOSName())
-	fmt.Print(*getOSVersion())
+	err:=errors.New("123")
+	fmt.Printf("%T,%s",err.Error(),err.Error())
+	if strings.Contains(err.Error(),"123"){
+		fmt.Println(1)
+	}
 }
