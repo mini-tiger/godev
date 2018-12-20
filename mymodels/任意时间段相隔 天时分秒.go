1e3d856c9348a3cff352236d406d2e6c3cd1d521
tj <61566027@163.com>
2018-12-20 19:56:14 +0800

    默认的

--- mymodels/任意时间段相隔 天时分秒.go
+++ mymodels/任意时间段相隔 天时分秒.go
@@ -16,22 +16,22 @@ func main() {
 	fmt.Println(time.Unix(start, 0).Format(timeLayout))
 	//time.Sleep(time.Duration(5)*time.Second)
 	endtime := time.Now().Unix()
-	d, h, m, s := get(endtime - start)
+	d, h, m, s := GetTime(endtime - start)
 	fmt.Printf("%d天%d小时%d分钟%d秒", d, h, m, s)
 }
 
-func get(sinter int64) (d, h, m, s int64) {
+func GetTime(sinter int64) (d, h, m, s int64) {
 	if day, interval := getBasic(sinter, 86400); day > 0 {
 		d = day
-		h, m = oneday(interval)
+		h, m = oneDay(interval)
 	} else {
-		h, m = oneday(interval)
+		h, m = oneDay(interval)
 	}
 	s = sinter - (d * 86400) - (h * 3600) - (m * 60)
 	return
 }
 
-func oneday(interval int64) (h, m int64) { // 一天内的小时分钟
+func oneDay(interval int64) (h, m int64) { // 一天内的小时分钟
 	h = interval / 3600
 	if h == 0 {
 		m = interval / 60
