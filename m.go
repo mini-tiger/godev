6e497a3711839f27773f621fc7ed738dc2e736b6
tj <61566027@163.com>
2019-02-19 21:24:00 +0800

    默认的

--- m.go
+++ m.go
@@ -1,74 +1,130 @@
 package main
 
 import (
-	"os/exec"
-
+	"encoding/json"
 	"fmt"
-	"golang.org/x/text/encoding/simplifiedchinese"
+	"github.com/astaxie/beego/httplib"
+	"io/ioutil"
 	"log"
-	"strings"
+	"time"
 )
-
-type Charset string
-
-const (
-	UTF8    = Charset("UTF-8")
-	GB18030 = Charset("GB18030")
-)
-
-func ConvertByte2String(byte []byte, charset Charset) string {
-
-	var str string
-	switch charset {
-	case GB18030:
-		var decodeBytes, _ = simplifiedchinese.GB18030.NewDecoder().Bytes(byte)
-
-		str = string(decodeBytes)
-	case UTF8:
-		fallthrough
-	default:
-		str = string(byte)
-	}
-
-	return str
-}
 
 func main() {
-	cmd_string := fmt.Sprintf("tracert -d -h 5 -w 15 -4 www.baidu.com")
-	fmt.Println("cmd", "/c", cmd_string)
+	//sendjson() //发送并接收JSON
+	//sendForm()
+	get()
+}
 
-	cmd := exec.Command("cmd", "/c", cmd_string)
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
-	stdout, err := cmd.CombinedOutput()
+type Hostgroup struct {
+	Id          int    `json:"id"`
+	Grp_name    string `json:"grp_name"`
+	Create_user string `json:"create_user"`
+}
+
+func get() {
+	req := httplib.NewBeegoRequest("http://10.240.81.85:8080/api/v1/hostgroup", "GET")
+	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
+
+	resp, err := req.Response()
+	//fmt.Println(resp,err)
+
 	if err != nil {
 		log.Println(err)
-
 	}
-	ss := ConvertByte2String(stdout, GB18030)
-	//fmt.Println(ss)
-	//fmt.Println("===================================")
-	ss = strings.TrimSpace(ss)
-	ss = strings.Trim(ss, "\n")
-	//fmt.Println(strings.Index(ss,"路由:"))
-	s_sub := "路由"
-	if strings.Index(ss, "路由:") != -1 {
-		s_sub = fmt.Sprintf("%s:", s_sub)
-	}
-	ss1 := strings.Split(ss, s_sub)
-
-	ss2 := strings.Split(ss1[1], "跟踪完成。")
-	//ss = strings.Trim(ss2[0], "\n")
-	//ss = strings.Trim(ss2[0], "\r")
-	ss = strings.TrimSpace(ss2[0])
-
-	ss3 := strings.Split(ss, "\r\n")
-	fmt.Println(len(ss3))
-	for _, v := range ss3 {
-		sl := strings.Fields(v)
-		temp := sl[len(sl)-1]
-		if strings.Contains(temp, "请求超时") {
-			continue
+	if resp.StatusCode == 200 {
+		fmt.Println(200)
+		respBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
 		}
-		fmt.Println(temp)
+		var respdata []Hostgroup
+		err = json.Unmarshal(respBytes, &respdata)
+		if err != nil {
+			log.Printf("json err:%s\n", err)
+		}
+		fmt.Println(respdata)
+	} else {
+		log.Println(resp.Body)
 	}
+
+}
+
+func sendForm() {
+	req := httplib.NewBeegoRequest("http://127.0.0.1:8080/", "GET")
+	//r,err:=httplib.Get("http://www.163.com/").Debug(true).Response()
+	//fmt.Println(req.Response())
+	//fmt.Println(r,err)
+	req.Header("Accept-Encoding", "gzip,deflate,sdch")
+	req.Header("Connection", "keep-alive")
+	req.Header("Content-type", "application/json")
+	req.Header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36")
+
+	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
+	req.Param("username", "astaxie")
+	req.Param("password", "123456")
+	//resp,err:=req.Response()
+	//if err!=nil{
+	//	log.Println(err)
+	//}
+	//if resp.StatusCode ==200{
+	//	resp.Body
+	//}
+	s, err := req.String()
+	if err == nil {
+		fmt.Println(s)
+	}
+}
+
+func sendjson() {
+	req := httplib.NewBeegoRequest("http://127.0.0.1:8080/test/", "get")
+	//r,err:=httplib.Get("http://www.163.com/").Debug(true).Response()
+	//fmt.Println(req.Response())
+	//fmt.Println(r,err)
+	req.Header("Accept-Encoding", "gzip,deflate,sdch")
+	req.Header("Connection", "keep-alive")
+	req.Header("Content-type", "application/json")
+	req.Header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36")
+
+	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
+	u := User{"abc", 11}
+
+	req.JSONBody(u) //todo 直接发送json
+
+	//方法一 todo 返回解析body内容
+	//resp,err:=req.Response()
+	////fmt.Println(resp,err)
+	//
+	//if err!=nil{
+	//	log.Println(err)
+	//}
+	//if resp.StatusCode == 200{
+	//	fmt.Println(200)
+	//	respBytes, err := ioutil.ReadAll(resp.Body)
+	//	if err != nil {
+	//		fmt.Println(err.Error())
+	//		return
+	//	}
+	//	var respdata User
+	//	err=json.Unmarshal(respBytes,&respdata)
+	//	if err!=nil{
+	//		log.Printf("json err:%s\n",err)
+	//	}
+	//	fmt.Println(respdata)
+	//}else{
+	//	log.Println(resp.Body)
+	//}
+
+	//方法二 todo 返回的结果直接 JSON解析,不能查看 statucode
+	var respdata User
+	err := req.ToJSON(&respdata)
+	if err != nil {
+		log.Println("resp err:%s\n", err)
+	}
+	fmt.Println(respdata)
 }
--- mymodels/爬虫/start.bat
+++ mymodels/爬虫/start.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd D:\work\project-dev\src\godev\mymodels\爬虫
-桃花族.exe -url http://thzbt.co/ -proxy -maxold 12
+桃花族.exe -url http://thzvv.net/ -proxy -maxold 4
 pause
