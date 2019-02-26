6e2a75699dc86c82a364a3aabc120f7301c1c495
tao-work-centos6.x <61566027@163.com>
2019-02-26 16:13:42 +0800

    work

--- bao.go
+++ bao.go
@@ -6,17 +6,11 @@ import (
 	"fmt"
 	"os"
 	"bufio"
-	"time"
 )
 
 var ss=`
-User Access Verification
-
-Username: admin
-Password: 
-1F-bangong-1>en
-Password: 
-1F-bangong-1#show run | redirect tftp://10.240.81.86/10.240.80.11-20190226110$redirect tftp://10.240.81.86/10.240.80.11-201902261102         .cfg
+str == "\r" 
+CORE-4506#show run | redirect tftp://10.240.81.86/10.240.80.254-201902261443.$redirect tftp://10.240.81.86/10.240.80.254-201902261443.c         fg
 !
 
 
@@ -25,46 +19,19 @@ Password:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-CORE-4506#show run | redirect tftp://10.240.81.86/10.240.80.254-201902261443.^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H$redirect tftp://10.240.81.86/10.240.80.254-201902261443.c         ^H^H^H^H^H^H^H^H^Hfg^M
-!^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M
-CORE-4506#Trying 10.240.80.11...^M
-Connected to 10.240.80.11.^M
-Escape character is '^]'.^M
-User Access Verification^M
-Username: admin^M
-Password: ^M
-1F-bangong-1>en^M
-Password: ^M
-1F-bangong-1#show run | redirect tftp://10.240.81.86/10.240.80.11-20190226144^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H^H$redirect tftp://10.240.81.86/10.240.80.11-201902261443         ^H^H^H^H^H^H^H^H^H.cfg^M
-!^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M^M
-1F-bangong-1#Trying 10.240.80.12...^M
-Connected to 10.240.80.12.^M
-Escape character is '^]'.^M
-User Access Verification^M
-Username: admin^M
-Password: ^M
-Connection closed by foreign host.   
+122222222222   
 `
 
 func main() {
 	//s,_ := StrTools.StrTrims(ss)
-	//StrTrims(ss)
-	DeleteBlankFile("c:\\run.log","c:\\11112.log")
+	//fmt.Println(StrTrims(ss))
+	DeleteBlankFile("/root/run.log","1112.log")
 }
 func StrTrims(ss string) (d string,e error)  {
 	dest := bytes.NewBuffer([]byte{})
-	ssb := bytes.NewBuffer([]byte(ss))
+	//ssb := bytes.NewBuffer([]byte(ss))
+	src,_:=os.OpenFile("/root/run.log", os.O_RDONLY, 0666)
+	ssb := bufio.NewReader(src)
 	for {
 		str, err := ssb.ReadString('\n')
 		if err != nil {
@@ -81,11 +48,14 @@ func StrTrims(ss string) (d string,e error)  {
 		if 0 == len(str) || str == "\r\n" {
 			continue
 		}
-		if 1 == len(str) || str == "\n" {
-			fmt.Println(str)
+		if 1 == len(str) && str == "\n" {
+			//fmt.Println(str)
 			continue
 		}
-		//fmt.Print(str)
+		if 1 == len(str) && str == "\r" {
+			//fmt.Println(str)
+			continue
+		}
 		dest.WriteString(str)
 	}
 	return dest.String(),nil
@@ -109,10 +79,10 @@ func DeleteBlankFile(srcFilePah string, destFilePath string) error {
 		str, _ := srcReader.ReadString('\r')
 		fmt.Println(str)
 		fmt.Println(str=="\r")
-		fmt.Println(str=="\n")
-		fmt.Println(str=="\r\n")
+		//fmt.Println(str=="\n")
+		//fmt.Println(str=="\r\n")
 		fmt.Println(len(str))
-		time.Sleep(2*time.Second)
+		//time.Sleep(1*time.Second)
 		if err != nil {
 			if err == io.EOF {
 				fmt.Print("The file end is touched.")
@@ -122,7 +92,10 @@ func DeleteBlankFile(srcFilePah string, destFilePath string) error {
 			}
 		}
 		if 0 == len(str) || str == "\r\n" {
-			fmt.Println(str)
+			continue
+		}
+		if 1 == len(str) && (str == "\n" || str == "\r"){
+			//fmt.Println(str)
 			continue
 		}
 		//fmt.Print(str)
