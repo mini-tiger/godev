5d177d32901ae598e9295c39bfd4d14d8c683dea
tj-thinkpad <61566027@163.com>
2019-03-12 10:01:19 +0800

    work

--- first.go
+++ first.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"os/exec"
+	"os"
+	"path/filepath"
+	"fmt"
+)
+
 func main()  {
-	
+	fmt.Println(GetCurPath())
+}
+
+/*获取当前文件执行的路径*/
+func GetCurPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+
+	//得到全路径，比如在windows下E:\\golang\\test\\a.exe
+	path, _ := filepath.Abs(file)
+
+	rst := filepath.Dir(path)
+
+	return rst
 }
