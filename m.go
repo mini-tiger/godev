136d8f534e5633bf8e5d900443f40258c66703da
tao-work-centos6.x <61566027@163.com>
2019-03-15 13:01:14 +0800

    ubs 更新逻辑,update 更改脚本执行 返回输出

--- m.go
+++ m.go
@@ -1,130 +1,44 @@
 package main
 
 import (
-	"encoding/json"
+	"os/exec"
 	"fmt"
-	"github.com/astaxie/beego/httplib"
-	"io/ioutil"
-	"log"
 	"time"
+	"reflect"
+	"bytes"
 )
 
-func main() {
-	//sendjson() //发送并接收JSON
-	//sendForm()
-	get()
+func main()  {
+	run()
 }
 
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
+func run()  {
+	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("cd %s;./%s", "/home", "tomcat_start.sh"))
 
-type Hostgroup struct {
-	Id          int    `json:"id"`
-	Grp_name    string `json:"grp_name"`
-	Create_user string `json:"create_user"`
-}
-
-func get() {
-	req := httplib.NewBeegoRequest("http://10.240.81.85:8080/api/v1/hostgroup", "GET")
-	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
-
-	resp, err := req.Response()
-	//fmt.Println(resp,err)
-
-	if err != nil {
-		log.Println(err)
-	}
-	if resp.StatusCode == 200 {
-		fmt.Println(200)
-		respBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
+	go func(cmd *exec.Cmd) { // 实时监控判断脚本运行状态，打印日志
+		for i := 1; ; i++ {
+			var d int = 10
+			time.Sleep(time.Duration(d) * time.Second)
+			c := cmd.Process
+			v := reflect.ValueOf(c).Elem()
+			vn := v.FieldByName("isdone")
+			if (vn.Uint() == uint64(1)) {
+				fmt.Printf("脚本运行完成%s,运行参数:%+v\n", cmd.Args, c)
+				break
+			} else {
+				fmt.Printf("正在运行的脚本%s,已运行%d秒,运行参数:%+v\n", cmd.Args, i*d, c)
+			}
 		}
-		var respdata []Hostgroup
-		err = json.Unmarshal(respBytes, &respdata)
-		if err != nil {
-			log.Printf("json err:%s\n", err)
-		}
-		fmt.Println(respdata)
-	} else {
-		log.Println(resp.Body)
-	}
+	}(cmd)
 
-}
+	var out bytes.Buffer
+	cmd.Stdout = &out
 
-func sendForm() {
-	req := httplib.NewBeegoRequest("http://127.0.0.1:8080/", "GET")
-	//r,err:=httplib.Get("http://www.163.com/").Debug(true).Response()
-	//fmt.Println(req.Response())
-	//fmt.Println(r,err)
-	req.Header("Accept-Encoding", "gzip,deflate,sdch")
-	req.Header("Connection", "keep-alive")
-	req.Header("Content-type", "application/json")
-	req.Header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36")
-
-	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
-	req.Param("username", "astaxie")
-	req.Param("password", "123456")
-	//resp,err:=req.Response()
-	//if err!=nil{
-	//	log.Println(err)
-	//}
-	//if resp.StatusCode ==200{
-	//	resp.Body
-	//}
-	s, err := req.String()
-	if err == nil {
-		fmt.Println(s)
-	}
-}
-
-func sendjson() {
-	req := httplib.NewBeegoRequest("http://127.0.0.1:8080/test/", "get")
-	//r,err:=httplib.Get("http://www.163.com/").Debug(true).Response()
-	//fmt.Println(req.Response())
-	//fmt.Println(r,err)
-	req.Header("Accept-Encoding", "gzip,deflate,sdch")
-	req.Header("Connection", "keep-alive")
-	req.Header("Content-type", "application/json")
-	req.Header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36")
-
-	req.SetTimeout(time.Duration(10)*time.Second, time.Duration(10)*time.Second)
-	u := User{"abc", 11}
-
-	req.JSONBody(u) //todo 直接发送json
-
-	//方法一 todo 返回解析body内容
-	//resp,err:=req.Response()
-	////fmt.Println(resp,err)
-	//
-	//if err!=nil{
-	//	log.Println(err)
-	//}
-	//if resp.StatusCode == 200{
-	//	fmt.Println(200)
-	//	respBytes, err := ioutil.ReadAll(resp.Body)
-	//	if err != nil {
-	//		fmt.Println(err.Error())
-	//		return
-	//	}
-	//	var respdata User
-	//	err=json.Unmarshal(respBytes,&respdata)
-	//	if err!=nil{
-	//		log.Printf("json err:%s\n",err)
-	//	}
-	//	fmt.Println(respdata)
-	//}else{
-	//	log.Println(resp.Body)
-	//}
-
-	//方法二 todo 返回的结果直接 JSON解析,不能查看 statucode
-	var respdata User
-	err := req.ToJSON(&respdata)
-	if err != nil {
-		log.Println("resp err:%s\n", err)
-	}
-	fmt.Println(respdata)
+	var e bytes.Buffer
+	cmd.Stderr = &e
+	//cmd.Start()
+	//buf, _ := cmd.CombinedOutput()
+	cmd.Run()
+	fmt.Println(out.String())
+	fmt.Println(e.String())
 }
