85a4d963c04f333bf385a56c6c06b66399ad6404
tj-thinkpad <61566027@163.com>
2019-07-05 10:06:19 +0800

    work

--- bao.go
+++ bao.go
@@ -1,14 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	a := 10
+
+type A struct{
+	AA []int
+	BB map[int]int
+	Ab AB
+}
+
+
+type AB struct{
+	ABAB []int
+}
+
+
+func (a *A)getArry() []int {
+	return a.AA
+}
+
+func (ab *AB)getArry() []int {
+	return ab.ABAB
+}
+
+
+func main () {
+	m1:=[]int{1,1,2,2,3,4,5,1}
+	var a A=A{m1,map[int]int{1:1},AB{m1}}
 	fmt.Println(a)
-	//前置补0
-	fmt.Printf("%02d", a)
-	fmt.Println("")
-	fmt.Printf("%0*d", 3, a)
+	fmt.Println(a.getArry())
+	fmt.Println(a.Ab.getArry())
 }
