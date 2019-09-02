298ebaddd026ed5240fa8b48bcfb3603e51a967a
tao-work-centos6.x <61566027@163.com>
2019-09-02 10:12:06 +0800

    test push code

---
+++ mymodels/发送信号关闭程序.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func main() {
+	cmd := exec.Command("/bin/sh", "-c", "watch date > date.txt")
+	// Go会将PGID设置成与PID相同的值
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	start := time.Now()
+	time.AfterFunc(3*time.Second, func() { syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL) })
+	err := cmd.Run()
+	fmt.Printf("pid=%d duration=%s err=%s\n", cmd.Process.Pid, time.Since(start), err)
+}
