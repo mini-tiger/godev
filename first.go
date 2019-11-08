6bf4426e82db387111c7c69a2fdfa79aecc97d3b
tao-work-centos6.x <61566027@163.com>
2019-11-08 20:17:23 +0800

    work

--- first.go
+++ first.go
@@ -1,151 +1,44 @@
 package main
-
 import (
+	"errors"
+	"flag"
 	"fmt"
-	"sort"
-	"sync"
+	"os"
+	"path/filepath"
 )
 
-type LinkedMap struct {
-	sync.RWMutex
-	MData map[string]interface{}
-	MLink map[int]string
-}
+func getFileList(path string) ([]string,error){
+	files := make([]string,0)
 
-func NewLinkedMap() *LinkedMap {
-	return &LinkedMap{
-		MData: make(map[string]interface{}),
-		MLink: make(map[int]string),
+	f,err:=os.Stat(path)
+	if err!=nil {
+		return files,errors.New(fmt.Sprintf("path: %s ,Err:%s",path,err))
 	}
-}
-func (this *LinkedMap) Put(key string, val interface{}) {
-	this.Lock()
-	this.MData[key] = val
-	this.Unlock()
-	this.MLink[this.Max()] = key
-
-}
-
-func (this *LinkedMap) Max() int {
-	this.RLock()
-	defer func() {
-		this.RUnlock()
-
-	}()
-	var keys []int
-	for k, _ := range this.MLink {
-		keys = append(keys, k)
+	if !f.IsDir(){
+		return files,errors.New(fmt.Sprintf("path: %s ,Not Dir",path))
 	}
-	if len(keys) == 0 {
-		return 1
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
-	//fmt.Printf("current max %d\n", keys[0]+1)
-	return keys[0] + 1
-}
 
-func (this *LinkedMap) Get(key string) (interface{}, bool) {
-	this.RLock()
-	val, exists := this.MData[key]
-	this.RUnlock()
-	return val, exists
-}
 
-func (this *LinkedMap) Remove(key string) {
-	this.Lock()
-	delete(this.MData, key)
-	for lkey, value := range this.MLink {
-		if value == key {
-			delete(this.MLink, lkey)
+	err = filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if ( f == nil ) {
+			return err
 		}
-	}
-	this.Unlock()
-}
-
-func (this *LinkedMap) GetAndRemove(key string) (v interface{}, b bool) {
-	this.RLock()
-	v, b = this.MData[key]
-	this.RUnlock()
-	this.Remove(key)
-	return
-}
-func (this *LinkedMap) Clear() {
-	this.Lock()
-	this.MData = make(map[string]interface{})
-	this.MLink = make(map[int]string)
-	this.Unlock()
-}
-
-func (this *LinkedMap) Keys() []string {
-	this.RLock()
-	defer this.RUnlock()
-
-	keys := make([]string, 0)
-	for key, _ := range this.MData {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-func (this *LinkedMap) ContainsKey(key string) bool {
-	this.RLock()
-	_, exists := this.MData[key]
-	this.RUnlock()
-	return exists
-}
-
-func (this *LinkedMap) Size() int {
-	this.RLock()
-	len := len(this.MData)
-	this.RUnlock()
-	return len
-}
-
-func (this *LinkedMap) IsEmpty() bool {
-	this.RLock()
-	empty := (len(this.MData) == 0)
-	this.RUnlock()
-	return empty
-}
-func (this *LinkedMap) SortLinkMap() (keys []string) {
-	linkdata := make([]int, 0)
-	this.RLock()
-	for k, _ := range this.MLink {
-		linkdata = append(linkdata, k)
-	}
-	if len(linkdata) == 0 {
-		return
-	}
-	sort.Sort(sort.IntSlice(linkdata))
-	//fmt.Printf("%+v\n",this.MLink)
-	//fmt.Printf("%+v\n",linkdata)
-	for _, keynum := range linkdata {
-		keys = append(keys, this.MLink[keynum])
-	}
-	//fmt.Printf("%+v\n",keys)
-	this.RUnlock()
-	return
-}
-
-func main() {
-	a := NewLinkedMap()
-	a.Put("a", 1)
-	a.Put("b", 2)
-	a.Put("c", 3)
-	//fmt.Println(a.Max())
-	//fmt.Printf("%+v\n", a.MData)
-	//fmt.Printf("%+v\n", a.MLink)
-	for _, key := range a.SortLinkMap() {
-		//fmt.Println(key)
-		if v, e := a.Get(key); e {
-			fmt.Printf("key:%s,value:%v\n", key, v)
+		if f.IsDir() {
+			return nil
 		}
+		files= append(files,filepath.Join(path,f.Name()))
+		return nil
+	})
+	if err != nil {
+		return files,errors.New(fmt.Sprintf("path: %s ,err:%s",path,err))
 	}
+	return files,nil
+}
 
-	for _, key := range a.SortLinkMap() {
-		//fmt.Println(key)
-		if v, e := a.Get(key); e {
-			fmt.Printf("key:%s,value:%v\n", key, v)
-		}
-	}
+func main(){
+	flag.Parse()
+	//root := flag.Arg(0)
+	f,e:=getFileList("/home/go/GoDevEach/works/haifei/syncHtml/htmlData/")
+	fmt.Println(f)
+	fmt.Println(e)
 }
