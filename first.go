0120070a4d21da76ae9298cdcd511d0e9af62d2a
taojun <61566027@163.com>
2019-11-14 23:06:27 +0800

    默认的

--- first.go
+++ first.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"gitee.com/taojun319/tjtools/file"
-	"os"
-	"path/filepath"
-	"strings"
+	"runtime"
 )
 
-func GetFatherDir(sf string) (string, error) {
-	if !file.IsExist(sf) {
-		return "", errors.New(sf + "Not Exist")
-	}
-	dirList := strings.Split(sf, string(os.PathSeparator))
-	if len(dirList) <= 1 {
-		return sf, nil
-	}
-	return strings.Join(dirList[0:len(dirList)-1], string(os.PathSeparator)), nil
-}
-
 func main() {
-	a := "D:\\work\\project-dev\\GoDevEach\\works\\haifei\\syncHtml\\HisData\\10.135.13.164_5day_Chinese.html"
-	fmt.Println(filepath.Base(a))
-	fmt.Println(filepath.Split(a))
-	dir, filename := filepath.Split(a)
-	fmt.Println(dir)
-	fmt.Println(filepath.Dir(dir))
-	fmt.Println(filepath.Dir(filename))
-	fmt.Println(filepath.Base(dir))
-	fmt.Println(strings.Split(a, string(os.PathSeparator)))
-	fmt.Println(strings.Split(a, "D:\\work\\project-dev\\GoDevEach\\works\\haifei\\syncHtml\\HisData\\"))
-
-	fmt.Println(GetFatherDir(a))
-	if b, e := GetFatherDir(a); e == nil {
-		fmt.Println(GetFatherDir(b))
-	}
-
-	fmt.Println(GetFatherDir("d:\\"))
-	fmt.Println(filepath.Abs("d:\\"))
-
+	Foo()
+}
+func Foo() {
+	fmt.Printf("我是 %s, %s 在调用我!\n", printMyName(), printCallerName())
+	Bar()
+}
+func Bar() {
+	fmt.Printf("我是 %s, %s 又在调用我!\n", printMyName(), printCallerName())
+}
+func printMyName() string {
+	pc, a, b, c := runtime.Caller(1)
+	fmt.Println(pc, a, b, c)
+	return runtime.FuncForPC(pc).Name()
+}
+func printCallerName() string {
+	pc, _, _, _ := runtime.Caller(2)
+	return runtime.FuncForPC(pc).Name()
 }
