a8709b5cb6d0373a17a513182df8d7c63e587a48
tao-work-centos6.x <61566027@163.com>
2019-11-15 15:27:57 +0800

    work

--- first.go
+++ first.go
@@ -1,26 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+import "fmt"
 
-func main() {
-	Foo()
-}
-func Foo() {
-	fmt.Printf("我是 %s, %s 在调用我!\n", printMyName(), printCallerName())
-	Bar()
-}
-func Bar() {
-	fmt.Printf("我是 %s, %s 又在调用我!\n", printMyName(), printCallerName())
-}
-func printMyName() string {
-	pc, a, b, c := runtime.Caller(1)
-	fmt.Println(pc, a, b, c)
-	return runtime.FuncForPC(pc).Name()
-}
-func printCallerName() string {
-	pc, _, _, _ := runtime.Caller(2)
-	return runtime.FuncForPC(pc).Name()
+func main()  {
+	s:=make(map[string]string,10)
+
+	fmt.Printf("%+v\n",s)
 }
