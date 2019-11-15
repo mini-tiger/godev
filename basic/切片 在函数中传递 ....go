6033c0bfc297f7967abf56e7cad1000a82db4d1d
tao-work-centos6.x <61566027@163.com>
2019-11-15 12:24:29 +0800

    work

--- basic/切片 在函数中传递 ....go
+++ basic/切片 在函数中传递 ....go
@@ -9,10 +9,14 @@ func e() error {
 func main()  {
 	fmt.Printf("%T,%v\n",e(),[]string{"a","b"})
 	revier_slice([]string{"aa","bb"}...)
+	Test1(1,"2")
 	//"后面加上三个点"
 }
 
 func revier_slice(s ...string)  {
 	//接收一端可以不用  []string
-	fmt.Printf("%T,%v",s,s)
+	fmt.Printf("%T,%v\n",s,s)
+}
+func Test1(t ...interface{})  {
+	fmt.Println(t)
 }
