45083f28af9aab7ca5bb7f3c78123ab0973667b5
tao-work-centos6.x <61566027@163.com>
2019-11-18 17:21:05 +0800

    work

--- first.go
+++ first.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 )
 
-func main()  {
-	s:="default//oracle/client"
+func main() {
+	m := make(map[string]interface{}, 0) //make 建立的是指针
+	//mm:=&m
+	m = map[string]interface{}{"a": "a", "b": "b"}
 
-	var dd =regexp.MustCompile("/.*")
-	match :=dd.FindString(s)
-	fmt.Println(match)
+	fmt.Printf("第一次打印原始地址：%p,值：%v,保存原始地址的指针地址:%p\n", m, m, &m)
 
+	Test1(&m)
+	Test2(m)
+	fmt.Printf("第二次打印原始地址：%p,值：%v,保存原始地址的指针地址:%p\n", m, m, &m)
+	m["a"] = "aaa"
+	fmt.Printf("第三次打印原始地址：%p,值：%v,保存原始地址的指针地址:%p\n", m, m, &m)
+}
+
+func Test1(m *map[string]interface{}) {
+	fmt.Printf("Test1函数内第一次打印地址：%p,值%v\n", m, *m)
+	(*m)["a"] = "aa"
+	fmt.Printf("Test1函数内第二次打印地址%p,值%v\n", m, *m)
+}
+
+func Test2(m map[string]interface{}) {
+	fmt.Printf("Test2函数内第一次打印地址%p,值%v,内存地址:%p\n", m, m, &m)
+	m["a"] = "aaaa"
+	fmt.Printf("Test2函数内第二次打印地址%p,值%v，内存地址:%p\n", m, m, &m)
 }
