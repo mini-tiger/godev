157fd8f1c2ddf2c05719ced85597584440972a0b
tao-work-centos6.x <61566027@163.com>
2019-12-02 16:28:23 +0800

    work

--- first.go
+++ first.go
@@ -1,14 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
+func split(s *string, maxlen int) (*string) {
+	if len(*s) > maxlen {
+		(*s)=(*s)[0:maxlen-3]
+		return s
+	} else {
+		return s
+	}
+
+}
+
 
 func main()  {
-	s:="dd d \n   ddd "
-	ss:=&s
-	fmt.Println(*ss)
-
-	fmt.Println(strings.ReplaceAll(strings.ReplaceAll(*ss,"\n","")," ",""))
+	a:="abcabc11111ddddd"
+	fmt.Printf("%v,%v\n",&a,a)
+	split(&a,10)
+	fmt.Printf("%v,%v\n",&a,a)
 }
