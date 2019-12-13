4ddb59e74a8b0228e770889bffa7c57ec2042e2a
tao-work-centos6.x <61566027@163.com>
2019-12-13 12:55:35 +0800

    work

--- basic/并发/atomic原子性代替锁.go
+++ basic/并发/atomic原子性代替锁.go
@@ -9,6 +9,10 @@ import (
 	"sync"
 	"math/rand"
 )
+/*
+fixme 原子性操作由底层硬件支持，锁是由操作系统API实现，前者效率更高
+
+*/
 
 var data *string
 
@@ -40,7 +44,7 @@ func main() {
 		go func() {
 			time.Sleep(time.Second * time.Duration(rand.Intn(1000)) / 1000)
 
-			log.Println(Data())
+			log.Println(Data()) //xxx 提取数据
 			wg.Done()
 		}()
 	}
@@ -51,12 +55,15 @@ func main() {
 			s := fmt.Sprint("#", i)
 			log.Println("====", s)
 
-			SetData(s)
+			SetData(s) // xxx 写入数据，在没有写完前 其它纯种提取不到数据
 			wg.Done()
 		}(i)
 	}
 
 	wg.Wait()
+
+
+	//
 
 	fmt.Println("final data = ", *data)
 }
--- first.go
+++ first.go
@@ -2,40 +2,61 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"log"
 	"time"
+	"unsafe"
+	"sync/atomic"
+	"sync"
+	"math/rand"
 )
 
-func formatTime(toBeCharge string) string {
-	//toBeCharge := "09/04/2019 11:03:16"
-	timeLayout := "01/02/2006 15:04:05" // 	月/日/年
+var data []string
 
-	//转化所需模板
-	loc, _ := time.LoadLocation("Local")                            //重要：获取时区
-	theTime, _ := time.ParseInLocation(timeLayout, toBeCharge, loc) //使用模板在对应时区转化为time.time类型
-	sr := theTime.Unix()                                            //转化为时间戳 类型是int64
-	//fmt.Println(theTime)                                            //打印输出theTime 2015-01-01 15:15:00 +0800 CST
-	fmt.Println(sr)
-	if sr<0{
-		timeLayout = "02/01/2006 15:04:05" // 	月/日/年
-		theTime, _ = time.ParseInLocation(timeLayout, toBeCharge, loc) //使用模板在对应时区转化为time.time类型
-		sr = theTime.Unix()
+// get data atomically
+func Data() string {
+	p := (*string)(atomic.LoadPointer(
+		(*unsafe.Pointer)(unsafe.Pointer(&data)),
+	))
+	if p == nil {
+		return ""
+	} else {
+		return *p
 	}
-	return strconv.FormatInt(sr, 10)
 }
 
+// set data atomically
+func SetData(d string) {
+	atomic.StorePointer(
+		(*unsafe.Pointer)(unsafe.Pointer(&data)),
+		unsafe.Pointer(&d),
+	)
+}
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(200)
 
-	//a:=make([]string,3)
-	b:=make([]string,3)
-	b[1]="1"
-	b[0]="0"
-	b[2]="2"
-	index:=1
-	b=append(b[:index],b[index+1:]...)
-	//fmt.Println(a)
-	fmt.Println(b,len(b))
+	for range [100]struct{}{} {
+		go func() {
+			time.Sleep(time.Second * time.Duration(rand.Intn(1000)) / 1000)
 
+			log.Println(Data()) //xxx 提取数据
+			wg.Done()
+		}()
+	}
 
+	for i := range [100]struct{}{} {
+		go func(i int) {
+			time.Sleep(time.Second * time.Duration(rand.Intn(1000)) / 1000)
+			s := fmt.Sprint("#", i)
+			log.Println("====", s)
+
+			SetData(s) // xxx 写入数据，在没有写完前 其它纯种提取不到数据
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("final data = %v\n ", data)
 }
