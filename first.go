42d95c134af3359d9a03ce19fcd715421be565f2
tao-work-centos6.x <61566027@163.com>
2019-12-18 10:28:39 +0800

    work

--- first.go
+++ first.go
@@ -1,20 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func add(a,b int) int {
-	return a+b
+type A struct {
+	AA string
 }
 
-type  N struct {
-	A func(int,int) int
-}
+func main() {
+	// 初始化一个pool
+	pool := &sync.Pool{
+		// 默认的返回值设置，不写这个参数，默认是nil
+		New: func() interface{} {
+			return &A{}
+		},
+	}
 
-func main()  {
-	aa:=1
-	bb:=2
-	fmt.Println("=",add(aa,bb))
-	fmt.Printf("add pointer %p\n",add)
-	n:=N{add}
-	fmt.Println(n.A(aa,bb))
+	// 看一下初始的值，这里是返回0，如果不设置New函数，默认返回nil
+	init := pool.Get().(*A)
+	init.AA="a"
+	fmt.Println(init)
+
+	// 设置一个参数1
+	pool.Put(1)
+	pool.Put(2)
+
+	// 获取查看结果
+	num := pool.Get()
+	fmt.Println(num)
+
+	// 再次获取，会发现，已经是空的了，只能返回默认的值。
+	num = pool.Get()
+	fmt.Println(num)
 }
