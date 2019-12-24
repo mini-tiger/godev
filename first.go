337f876ea1404e55a36ab84d45efed52099f0142
tao-work-centos6.x <61566027@163.com>
2019-12-24 22:01:45 +0800

    work

--- first.go
+++ first.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"gitee.com/taojun319/tjtools/utils"
-	"haifei/syncHtml/g"
-	"log"
-
-	"time"
+	"fmt"
+	"path/filepath"
 )
 
 func ClearMap() {
@@ -14,12 +11,7 @@ func ClearMap() {
 }
 
 func main() {
-	startRunTime:=time.Now().Unix()
-	for {
-		sinter := time.Now().Unix() - startRunTime
-		utils.GetTime(&sinter, &d, &h, &m, &s)
-		log.Info("开始运行时间 %s,已经运行%d天%d小时%d分钟%d秒\n", time.Unix(startRunTime, 0).Format(g.TimeLayout), d, h, m, s)
-		time.Sleep(time.Duration(900) * time.Second)
-	}
+	f,e:=filepath.Glob("/home/go/GoDevEach/works/haifei/syncHtml/htmlData/*")
+	fmt.Println(f,e)
 
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	gitee.com/taojun319/godaemon v0.0.0-20180906150213-33be0b026b29
+	gitee.com/taojun319/tjtools v0.0.0-20191221034246-ba40be3f14d8 // indirect
 	github.com/mattn/go-oci8 v0.0.0-20191108001511-cbd8d5bc1da0 // indirect
 )
