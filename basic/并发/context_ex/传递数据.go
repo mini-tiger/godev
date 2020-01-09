8e7582c7d9124e925936d784914711673c005e39
tao-work-centos6.x <61566027@163.com>
2020-01-09 13:01:26 +0800

    work

---
+++ basic/并发/context_ex/传递数据.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+var key1 string = "name1"
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	//附加值
+	valueCtx1 := context.WithValue(ctx, key1, "[goroutine1]")
+
+
+	go watch(valueCtx1)
+
+
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知 all goroutine停止")
+	cancel()
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+}
+
+func watch(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			//取出值
+			fmt.Println("监控退出，停止了...")
+			return
+		default:
+			//取出值
+			fmt.Println(ctx.Value(key1), "goroutine监控中...")
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
---
+++ basic/并发/context_ex/多个Goroutine控制.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+
+
+	go watch(ctx,"goroutine1")
+	go watch(ctx,"goroutine2")
+
+
+	time.Sleep(10 * time.Second)
+	fmt.Println("可以了，通知 all goroutine停止")
+	cancel()
+	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	time.Sleep(5 * time.Second)
+}
+
+func watch(ctx context.Context,name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			//取出值
+			fmt.Println("监控退出，停止了...")
+			return
+		default:
+			//取出值
+			fmt.Println(name, "goroutine监控中...")
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
