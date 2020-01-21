357d7c4dc10640a7c89c21767d3dc823c8b95173
tao-work-centos6.x <61566027@163.com>
2020-01-21 14:35:39 +0800

    work

--- basic/并发/sync_ex/once.go
+++ basic/并发/sync_ex/once.go
@@ -1,15 +1,28 @@
 package main
 
-//!+
-import "sync"
 import (
 	"fmt"
-	// "time"
+	"sync"
 	"time"
 )
-var done = make(chan struct{},0)
+
+var done = make(chan struct{}, 0)
 
 func main() {
+
+	//for i := 0; i < 10; i++ {
+	//	<-done
+	//	fmt.Printf("第 %d 次<-done\n",i)
+	//	time.Sleep(time.Duration(1)*time.Second)
+	//}
+	//select {}
+	for i := 0; i < 10; i++ { //循环10次，但do方法只会在第一次调用时执行
+		tt()
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+
+}
+func tt() {
 
 	var once *sync.Once = new(sync.Once)
 	onceBody := func() {
@@ -17,17 +30,8 @@ func main() {
 		done <- struct{}{}
 	}
 
-	for i := 0; i < 10; i++ { //循环10次，但do方法只会在第一次调用时执行
-		go func() {
-			once.Do(onceBody) //Do方法当且仅当第一次被调用时才执行函数
-		}()
-		time.Sleep(time.Duration(1)*time.Second)
-	}
+	go func() {
+		once.Do(onceBody) //Do方法当且仅当第一次被调用时才执行函数
+	}()
 
-	for i := 0; i < 10; i++ {
-		<-done
-		fmt.Printf("第 %d 次<-done\n",i)
-		time.Sleep(time.Duration(1)*time.Second)
-	}
-	select {}
 }
