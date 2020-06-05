8e193bb8c630c304728a39f7ab850856e125fa4c
tao-work-centos6.x <61566027@163.com>
2020-06-05 15:38:56 +0800

    work

--- models/unsafe/pointer.go
+++ models/unsafe/pointer.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync/atomic"
 	"unsafe"
 )
 
 func main() {
+
+	// 整形5 的地址 强制转换为float
+	num:=5
+	floatnum :=(*float32)(unsafe.Pointer(&num))
+	fmt.Println(floatnum)
+	fmt.Println(*floatnum)
+
+
+	str:="A"
+	str1:="B"
+	//var pstr,pstr1 unsafe.Pointer
+	//var pstr1 unsafe.Pointer
+	pstr:=unsafe.Pointer(&str)
+	pstr1:=unsafe.Pointer(&str1)
+	fmt.Printf("转换前s的值:%s,addr:%p\n",str,&str)
+	fmt.Printf("转换前s1的值:%s,addr:%p\n",str1,&str1)
+	fmt.Printf("转换前pstr的值:%v,addr:%p\n",pstr,&pstr)
+	fmt.Printf("转换前pstr1的值:%v,addr:%p\n",pstr1,&pstr1)
+	//(string(pstr))
+	fmt.Println("=========================================================")
+	atomic.StorePointer(&pstr,pstr1) // pstr1 中的值地址 存储到 ＊pstr
+	fmt.Printf("转换后s的值:%s,无变化addr:%p\n",str,&str)
+	fmt.Printf("转换后s1的值:%s,无变化addr:%p\n",str1,&str1)
+	fmt.Printf("转换后pstr的值:%v,addr:%p\n",*(*string)(pstr),&pstr)
+	fmt.Printf("转换后pstr1的值:%v,addr:%p\n",*(*string)(pstr1),&pstr1)
+
+
+	// xxx 通过偏移量 修改 结构体
 	s := struct {
 		a byte
 		b byte
