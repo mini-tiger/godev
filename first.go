88c5a335db54adb6d211df3e2ee33c5fb242109a
tao-work-centos6.x <61566027@163.com>
2020-06-18 12:24:01 +0800

    增加END TIME

--- first.go
+++ first.go
@@ -2,50 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync/atomic"
+	"unsafe"
 )
 
-type Semaphore struct {
-	bufSize int
-	channel chan int8
-}
+func main()  {
+	var i int32
+	var i32 uint16
+	fmt.Printf("%v,%d\n",&i,i)
+	atomic.AddInt32(&i,1)
+	fmt.Printf("%v,%d\n",&i,i)
 
-func NewSemaphore(concurrencyNum int) *Semaphore {
-	return &Semaphore{channel: make(chan int8, concurrencyNum), bufSize: concurrencyNum}
-}
-
-func (this *Semaphore) TryAcquire() bool {
-	select {
-	case this.channel <- int8(0):
-		return true
-	default:
-		return false
-	}
-}
-
-func (this *Semaphore) Acquire() {
-	this.channel <- int8(0)
-}
-
-func (this *Semaphore) Release() {
-	<-this.channel
-}
-
-func (this *Semaphore) AvailablePermits() int {
-	return this.bufSize - len(this.channel)
-}
-
-func main() {
-	se := NewSemaphore(5) // 有限的并发
-	for i := 0; i < 10000; i++ {
-		fmt.Printf("循环第%d次\n", i)
-		se.Acquire() // 没有可用这里会阻塞
-		go func(index int) {
-			defer se.Release()
-			fmt.Printf("可用并发数%d,循环%d次\n", se.AvailablePermits(),index)
-			time.Sleep(time.Duration(300000)*time.Millisecond)
-		}(i)
-		time.Sleep(time.Duration(500)*time.Millisecond)
-
-	}
+	fmt.Println(unsafe.Sizeof(i))
+	fmt.Println(unsafe.Sizeof(i32))
 }
