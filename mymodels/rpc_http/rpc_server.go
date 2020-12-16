dee2aa5b6ee4e9a211e763c7b116d021fe99fd9f
taojun <61566027@163.com>
2020-12-16 16:28:48 +0800

    update

--- mymodels/rpc_http/rpc_server.go
+++ mymodels/rpc_http/rpc_server.go
@@ -1,10 +1,11 @@
 package main
+
 // rpc  传送list可能会有问题
 import (
 	"godev/mymodels/rpc_http/common"
-	"net/rpc"
-	"net"
 	"log"
+	"net"
+	"net/rpc"
 	"time"
 	//_ "github.com/CodyGuo/godaemon"
 )
@@ -24,19 +25,15 @@ func main() {
 	//方式二
 	newServer := rpc.NewServer()
 	newServer.Register(ms)
-	ll, e := net.Listen("tcp", "127.0.0.1:1234") // any available address
+	ll, e := net.Listen("tcp", "0.0.0.0:1234") // any available address
 	if e != nil {
 		log.Fatalf("net.Listen tcp :0: %v", e)
 	}
 	newServer.HandleHTTP("/ext", "/bar")
-	for  {
+	for {
 		go newServer.Accept(ll)
 
-
-		time.Sleep(2*time.Second)
+		time.Sleep(2 * time.Second)
 	}
 
-
-
 }
-
