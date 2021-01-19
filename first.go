944348b35377d96d0c687b2a51aef7723c50ef46
taojun <61566027@163.com>
2021-01-19 16:20:32 +0800

    Default Changelist

--- first.go
+++ first.go
@@ -2,15 +2,11 @@ package main
 
 import "fmt"
 
-func fun() *int { //int类型指针函数
-	var tmp int = 1
-	defer fmt.Println(tmp * 2)
-	tmp = 2
-	return &tmp //返回局部变量tmp的地址
+type A struct {
+	AA string
 }
 
 func main() {
-	var p *int
-	p = fun()
-	fmt.Printf("%d\n", *p) //这里不会像C，报错段错误提示，而是成功返回变量V的值1
+	var a *A
+	fmt.Println(a)
 }
