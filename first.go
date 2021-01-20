255ee651d132cc44951beeeb17af8e8191adf247
taojun <61566027@163.com>
2021-01-20 17:26:36 +0800

    Default Changelist

--- first.go
+++ first.go
@@ -1,12 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	_ "net/http/pprof" // 会自动注册 handler 到 http server，方便通过 http 接口获取程序运行采样报告
+	"sync"
+	"time"
+)
 
-type A struct {
-	AA string
+func counter() {
+	list := []int{1}
+	c := 1
+	for i := 0; i < 100; i++ {
+		httpGet()
+		c = i + 1 + 2 + 3 + 4 - 5
+		list = append(list, c)
+	}
+	fmt.Println(c)
+	fmt.Println(list[0])
+}
+
+func work(wg *sync.WaitGroup) {
+	for {
+		counter()
+		time.Sleep(1 * time.Second)
+	}
+	wg.Done()
+}
+
+func httpGet() int {
+	queue := []string{"start..."}
+	resp, err := http.Get("http://www.163.com")
+	if err != nil {
+		// handle error
+	}
+
+	//defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+	queue = append(queue, string(body))
+	return len(queue)
 }
 
 func main() {
-	var a *A
-	fmt.Println(a)
+	flag.Parse()
+
+	//这里实现了远程获取pprof数据的接口
+	go func() {
+		log.Println(http.ListenAndServe("0.0.0.0:7777", nil)) // 监听7777端口
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 100; i++ {
+		go work(&wg)
+	}
+
+	wg.Wait()
+	time.Sleep(3 * time.Second)
 }
--- go.mod
+++ go.mod
@@ -9,11 +9,14 @@ require (
 	github.com/astaxie/beego v1.12.3
 	github.com/betacraft/yaag v1.0.0 // indirect
 	github.com/coocood/freecache v1.1.1 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
 	github.com/golang/protobuf v1.4.2
+	github.com/jessevdk/go-flags v1.4.0 // indirect
 	github.com/mattn/go-oci8 v0.0.0-20191108001511-cbd8d5bc1da0 // indirect
 	github.com/shirou/gopsutil v2.19.12+incompatible // indirect
+	github.com/uber/go-torch v0.0.0-20181107071353-86f327cc820e // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	google.golang.org/grpc v1.31.1
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
