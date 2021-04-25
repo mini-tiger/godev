a2740e412b0fe12b103a3897e9b7429dafaf7cb0
taojun <61566027@163.com>
2021-04-25 10:08:26 +0800

    Default Changelist

--- 面试题/panic嵌套_进阶.go
+++ 面试题/panic嵌套_进阶.go
@@ -21,7 +21,7 @@ func Panic() {
 	defer func() {
 		pv := recover()
 		fmt.Println("recover err :")
-		fmt.Println("11:", pv, reflect.TypeOf(pv)) // 8.   pv 收集的是最后一次 panic 的2, 如果没有recover 两次panic都打印
+		fmt.Println("main recover:", pv, reflect.TypeOf(pv)) // 8.   pv 收集的是最后一次 panic 的2, 如果没有recover 两次panic都打印
 	}()
 
 	foo() // 7. 这里触发了panic 不会在执行下面的代码，触发本Goroutine 下的defer
