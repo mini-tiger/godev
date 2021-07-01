72a99473750be558c2e7585c1f56a5539911af6b
taojun <61566027@163.com>
2021-07-01 11:36:40 +0800

    Default Changelist

--- 1.go
+++ 1.go
@@ -1,39 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"unsafe"
-)
+import "fmt"
 
-type All interface {
-	Get() int
-	Set()
-}
-type G interface {
-	Get() int
-}
-
-type S int
-
-func (s *S) Get() int {
-	return 1
-}
-func (s *S) Set() {
-	return
-}
-
-func byte2string(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-func string2byte(b string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&b))
-}
 func main() {
-	runtime.GOMAXPROCS(1)
-	var b []byte
-	b = []byte("Hello")
-	fmt.Println(byte2string(b))
+	a := 1
+	b := []int{1, 2, 3}
+	a, b[a-1] = 2, 2
+	fmt.Println(a)
+	fmt.Println(b)
+	aa := make(map[int]int, 0)
+	aa[1] = 1
+	delete(aa, 1)
+	aaxx()
+}
 
-	fmt.Println(string(string2byte(byte2string(b))))
+func aaxx() (x interface{}) {
+	fmt.Println(x == nil)
+	return
 }
