da9c4d08c0470bd473970153b2316f5d7f8df903
taojun <61566027@163.com>
2021-07-06 10:12:13 +0800

    Default Changelist

--- 1.go
+++ 1.go
@@ -1,20 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
-func main() {
-	a := 1
-	b := []int{1, 2, 3}
-	a, b[a-1] = 2, 2
-	fmt.Println(a)
-	fmt.Println(b)
-	aa := make(map[int]int, 0)
-	aa[1] = 1
-	delete(aa, 1)
-	aaxx()
+func worker_adder(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	// writes the sum to the go routines.
+	c <- sum // send sum to c
+	sw.Done()
+	//fmt.Println("end")
 }
 
-func aaxx() (x interface{}) {
-	fmt.Println(x == nil)
-	return
+var sw sync.WaitGroup
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	sw.Add(2)
+	// spin up a goroutine.
+	go worker_adder(s[:len(s)/2], c1)
+	// spin up a goroutine.
+	go worker_adder(s[len(s)/2:], c2)
+
+	x, y := <-c1, <-c2 // receive from c1 aND C2
+	//x, _:= <-c1
+	// 输出从channel获取到的值
+	fmt.Println(x, y)
+
+	fmt.Println(runtime.NumGoroutine())
+	sw.Wait()
+	fmt.Println(runtime.NumGoroutine())
 }
