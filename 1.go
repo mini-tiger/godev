e5b256fa75e13c70a02135cbdebacf9466c47dc9
taojun <61566027@163.com>
2021-08-14 00:48:55 +0800

    Default Changelist

--- 1.go
+++ 1.go
@@ -1,17 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+)
 
-type A struct {
-	A1 string
-	A2 int
-}
-
-
-func main()  {
-	var s *struct{
-		x [][31]byte
+func main() {
+	msg := "Tom:admin@123" //11个字符应该装成15个base64编码的字符
+	encoded := base64.StdEncoding.EncodeToString([]byte(msg))
+	fmt.Println(encoded) //SGVsbG8sd29ybGQ=,后面的=是作填充用的
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("decode error:", err)
+		return
 	}
-	fmt.Println(s.x[1024])
-	fmt.Println(reflect.)
+	fmt.Println(string(decoded)) //Hello,world
 }
