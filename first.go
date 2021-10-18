dc5d9a601621aa6cd790d77509789dca40a7e7bb
taojun <61566027@163.com>
2021-10-18 11:26:42 +0800

    Default Changelist

--- first.go
+++ first.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	netdata "github.com/netdata/go.d.plugin"
-	"net"
-)
-
-var (
-	// specify the range of trusted IP addresses
-	start = net.ParseIP("198.162.1.100")
-	end   = net.ParseIP("198.162.1.199")
+	"strconv"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println(netdata.ParseRange("192.168.0.0/16"))
+	var bodyMap map[string]interface{} = make(map[string]interface{}, 3)
+	for i := 0; i < 10; i++ {
+
+		bodyMap[strconv.Itoa(i)] = i
+		fmt.Println(unsafe.Sizeof(bodyMap), len(bodyMap))
+	}
+
 }
