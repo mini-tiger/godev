01d9a4b62bbe33130fb5d12800e46dc9afb9da72
tao-home <61566027@163.com>
2023-02-05 16:17:02 +0800

    Changes

--- first.go
+++ first.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
+var tc chan int = make(chan int, 100)
+
+func send() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("begin tc", time.Now())
+		tc <- 1
+		fmt.Println("end tc", time.Now())
+	}
+	close(tc)
+
+}
+func recv() {
+	for {
+		select {
+		case i, ok := <-tc:
+			fmt.Println(i, ok, time.Now())
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	t1 := time.Now()
-	time.Sleep(2)
-	t2 := time.Now()
-	fmt.Println(t2.Sub(t1))
-	fmt.Println(strings.TrimSpace(t2.Sub(t1).String()))
+	go recv()
+	go send()
+
+	select {}
 }
