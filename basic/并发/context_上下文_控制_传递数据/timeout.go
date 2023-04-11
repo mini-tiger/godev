fbf9ed49e3ad97d434f11ba7323db3ade6b3111e
tao.jun <tao.jun@neolink.com>
2023-04-11 16:30:12 +0800

    update

--- basic/并发/context_上下文_控制_传递数据/timeout.go
+++ basic/并发/context_上下文_控制_传递数据/timeout.go
@@ -10,31 +10,40 @@ import (
 var logg_timeout *log.Logger
 
 func someHandler_timeout() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second)) // xxx 5s超时 ctx.Done <-
-	go doStuff_timeout(ctx)
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(5*time.Second)) // xxx 5s超时 ctx.Done <-
 
-	//10秒后取消doStuff
-	time.Sleep(2 * time.Second)
-	cancel() //xxx  调用 直接 ctx.Done <-
+	finchan := make(chan struct{}, 0)
+	go func() {
+		defer func() {
+			finchan <- struct{}{}
+		}()
+		doStuff_timeout()
+
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logg_timeout.Printf("timeout %v", ctx.Err())
+			return
+		case <-finchan:
+			logg_timeout.Printf("finish")
+		}
+	}
 
 }
 
 //每1秒work一下，同时会判断ctx是否被取消了，如果是就退出
-func doStuff_timeout(ctx context.Context) {
+func doStuff_timeout() {
 	for {
 		time.Sleep(1 * time.Second)
-		select {
-		case <-ctx.Done():
-			logg_timeout.Printf("done")
-			return
-		default:
-			logg_timeout.Printf("work")
-		}
+		logg_timeout.Printf("%v\n", time.Now())
 	}
+
 }
 
 func main() {
 	logg_timeout = log.New(os.Stdout, "", log.Ltime)
 	someHandler_timeout()
-	logg_timeout.Printf("down")
+	//logg_timeout.Printf("down")
 }
---
+++ basic/并发/context_上下文_控制_传递数据/timeout2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type TaskFunc func(ctx context.Context) error
+
+func DoTask(task TaskFunc, timeout time.Duration) error {
+	// 创建一个具有设定超时时间的 context
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// 开始执行任务
+	errChan := make(chan error)
+	go func() {
+		errChan <- task(ctx) //xxx  任务函数直接返回 err 即可
+		close(errChan)
+	}()
+
+	// 等待任务结束
+	select {
+	case err := <-errChan:
+		return err
+	case <-ctx.Done():
+		fmt.Println("Task cancelled due to timeout:", ctx.Err())
+		return ctx.Err()
+	}
+}
+
+func main() {
+	// 测试任务
+	err := DoTask(func(ctx context.Context) error {
+		fmt.Println("Task started")
+		time.Sleep(6 * time.Second)
+		fmt.Println("Task completed")
+		return nil
+	}, 5*time.Second)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
